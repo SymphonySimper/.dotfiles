0ed684a2b24a1365de5a5d458586ce8d17e31043
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-02-29 22:54:08 +0530

    feat: set C-y to accept auto complete
    why teej and prime vouch for it

---
+++ packages/editor/nvim/config/lua/plugins/cmp.lua
@@ -0,0 +1,15 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.mapping = cmp.mapping.preset.insert({
+      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    })
+  end,
+}
