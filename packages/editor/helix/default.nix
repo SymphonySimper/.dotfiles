ebed9490d8025b6f191f34cac63ca7a6baf66d59
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-03-03 18:51:47 +0530

    feat(editor): add helix config

--- packages/editor/helix/default.nix
+++ packages/editor/helix/default.nix
@@ -1,17 +1,98 @@
 { config, pkgs, helix-flake, ... }:
 {
-  home.packages = with pkgs; [
-    helix-flake.packages.${pkgs.system}.default
-    # Lsp
-    ## svelte
-    nodePackages_latest.svelte-language-server
-    ## tailwind
-    tailwindcss-language-server
-    ## ts
-    typescript
-    nodePackages_latest.typescript-language-server
-    ## python
-    ruff-lsp
-    nodePackages_latest.pyright
-  ];
+  programs.helix = {
+    enable = true;
+    package = helix-flake.packages.${pkgs.system}.default;
+
+    settings = {
+      theme = "catppuccin_mocha";
+
+      editor = {
+        line-number = "relative";
+        cursorline = true;
+        true-color = true;
+        color-modes = true;
+        auto-format = true;
+        auto-save = true;
+        bufferline = "always";
+        scrolloff = 8;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        indent-guides = {
+          render = true;
+        };
+        lsp = {
+          enable = true;
+          display-inlay-hints = true;
+        };
+      };
+    };
+
+    extraPackages = with pkgs; [
+      # Lsp
+      ## svelte
+      nodePackages_latest.svelte-language-server
+      ## tailwind
+      tailwindcss-language-server
+      ## ts
+      typescript
+      nodePackages_latest.typescript-language-server
+      ## python
+      ruff-lsp
+      nodePackages_latest.pyright
+    ];
+
+    languages = {
+      language = [{
+        name = "svelte";
+        auto-format = true;
+        language-servers = [ "svelteserver" "tailwindcss-ls" ];
+      }
+
+        {
+          name = "html";
+          language-servers = [ "vscode-html-language-server" "tailwindcss-ls" ];
+        }
+
+        {
+          name = "css";
+          language-servers = [ "vscode-css-language-server" "tailwindcss-ls" ];
+        }
+
+        {
+          name = "jsx";
+          language-servers = [ "typescript-language-server" "tailwindcss-ls" ];
+        }
+
+        {
+          name = "tsx";
+          language-servers = [ "typescript-language-server" "tailwindcss-ls" ];
+        }
+
+        {
+          name = "python";
+          roots = [ "pyproject.toml" ];
+          language-servers = [ "pyright" "ruff" ];
+        }];
+
+      language-server = {
+        rust-analyzer.config.check = {
+          command = "clippy";
+        };
+
+        pyright = {
+          command = "pyright-langserver";
+          args = [ "--stdio" ];
+        };
+
+        ruff = {
+          command = "ruff-lsp";
+          config.settings.run = "onSave";
+        };
+      };
+    };
+  };
 }
