ce09a7342199d44cb0f92761b7672333bc9105b7
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-03-13 11:45:08 +0530

    feat(zellij): simple ui config

--- packages/cli/zellij.nix
+++ packages/cli/zellij.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
     settings = {
       theme = "catppuccin-mocha";
+      on_force_close = "detach";
+      simplified_ui = true;
+      default_shell = "${pkgs.zsh}/bin/zsh";
+      default_layout = "compact";
     };
   };
 }
