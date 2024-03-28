8fc46eb65b15fb01233c2d42355ab02b6c84e62c
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-03-28 21:37:00 +0530

    feat(nvim)!: disable all UI plugins

--- packages/editor/nvim/config/lua/plugins/disabled.lua
+++ packages/editor/nvim/config/lua/plugins/disabled.lua
@@ -1,5 +1,20 @@
-return {
-  { "rcarriga/nvim-notify", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
+local plugins = {
+  -- UI
+  "akinsho/bufferline.nvim",
+  "folke/noice.nvim",
+  "goolord/alpha-nvim",
+  "nvimdev/dashboard-nvim",
+  "rcarriga/nvim-notify",
+  "stevearc/dressing.nvim",
+  -- Utils
+  "folke/flash.nvim",
+  "folke/persistence.nvim",
+  "dstein64/vim-startuptime",
 }
+
+local disabedPlugins = {}
+for _, plugin in ipairs(plugins) do
+  table.insert(disabedPlugins, { plugin, enabled = false })
+end
+
+return disabedPlugins
