4f41b4366b491ac8c946b7cbc4e1b415823cb278
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-04-03 02:22:10 +0530

    feat(system): mv common essentail config to pc.nix

---
+++ system/pc.nix
@@ -0,0 +1,20 @@
+{ ... }: {
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+}
