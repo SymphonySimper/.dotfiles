580f9f09ed6ebb11511e66a44f2ceae60956d5ac
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-05-03 03:24:44 +0530

    feat(nvim): #1 add trouble keymaps

--- home/packages/editor/nvim/config/plugins/trouble.nix
+++ home/packages/editor/nvim/config/plugins/trouble.nix
@@ -1,5 +1,20 @@
 { ... }: {
-  programs.nixvim.plugins.trouble = {
-    enable = true;
+  programs.nixvim = {
+    plugins.trouble = {
+      enable = true;
+      settings = {
+        auto_close = true;
+        use_diagnostic_signs = true;
+      };
+    };
+
+    keymaps = [
+      {
+        action = "<CMD>TroubleToggle workspace_diagnostics<CR>";
+        key = "<leader>xx";
+        mode = "n";
+        options.desc = "Workspace Diagnostics (Trouble)";
+      }
+    ];
   };
 }
