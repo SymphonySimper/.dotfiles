884a5ce6190181b2d69acdb86063f362a946a952
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-05-02 18:44:52 +0530

    fix(nvim): #1 snippets

--- home/packages/editor/nvim/config/plugins/cmp.nix
+++ home/packages/editor/nvim/config/plugins/cmp.nix
@@ -16,6 +16,11 @@
               option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
             }
           ];
+          snippet.expand = ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
+            end
+          '';
           mapping = {
             "<C-n>" = "cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })";
             "<C-p>" = "cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })";
@@ -24,6 +29,10 @@
             "<C-Space>" = "cmp.mapping.complete()";
             "<C-e>" = "cmp.mapping.abort()";
             "<C-y>" = "cmp.mapping.confirm({ select = true })";
+          };
+
+          performance = {
+            max_view_entries = 10;
           };
         };
       };
