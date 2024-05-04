c18808266f762266adfd928e129759447b3333ef
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-05-04 20:47:01 +0530

    feat(nvim/telescope): add live_grep and fzf-native

--- home/packages/editor/nvim/config/plugins/telescope.nix
+++ home/packages/editor/nvim/config/plugins/telescope.nix
@@ -1,6 +1,16 @@
 { ... }: {
   programs.nixvim.plugins.telescope = {
     enable = true;
+
+    extensions = {
+      fzf-native = {
+        enable = true;
+        settings = {
+          case_mode = "smart_case";
+        };
+      };
+    };
+
     keymaps = {
       "<leader>f" = {
         action = "find_files";
@@ -10,6 +20,11 @@
       "<leader><space>" = {
         action = "buffers";
         options.desc = "Telescope find buffer";
+        mode = [ "n" "v" ];
+      };
+      "<leader>sg" = {
+        action = "live_grep";
+        options.desc = "Telescope find string";
         mode = [ "n" "v" ];
       };
     };
