61273dffab17dfbc35dd036df7a27035de3d08ba
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-05-05 14:15:45 +0530

    feat(nvim/telescope): add find siblings of current file

--- home/packages/editor/nvim/config/plugins/telescope.nix
+++ home/packages/editor/nvim/config/plugins/telescope.nix
@@ -1,32 +1,38 @@
-{ ... }: {
-  programs.nixvim.plugins.telescope = {
-    enable = true;
+{ utils, ... }: {
+  programs.nixvim = {
+    plugins.telescope = {
+      enable = true;
 
-    extensions = {
-      fzf-native = {
-        enable = true;
-        settings = {
-          case_mode = "smart_case";
+      extensions = {
+        fzf-native = {
+          enable = true;
+          settings = {
+            case_mode = "smart_case";
+          };
+        };
+      };
+
+      keymaps = {
+        "<leader>F" = {
+          action = "find_files";
+          options.desc = "Telescope find_files";
+          mode = [ "n" "v" ];
+        };
+        "<leader><space>" = {
+          action = "buffers";
+          options.desc = "Telescope find buffer";
+          mode = [ "n" "v" ];
+        };
+        "<leader>sg" = {
+          action = "live_grep";
+          options.desc = "Telescope find string";
+          mode = [ "n" "v" ];
         };
       };
     };
 
-    keymaps = {
-      "<leader>f" = {
-        action = "find_files";
-        options.desc = "Telescope find_files";
-        mode = [ "n" "v" ];
-      };
-      "<leader><space>" = {
-        action = "buffers";
-        options.desc = "Telescope find buffer";
-        mode = [ "n" "v" ];
-      };
-      "<leader>sg" = {
-        action = "live_grep";
-        options.desc = "Telescope find string";
-        mode = [ "n" "v" ];
-      };
-    };
+    keymaps = utils.mkKeymaps [
+      [ "function() require('telescope.builtin').find_files({ cwd = vim.fn.expand('%:p:h') }) end" "<leader>f" "n" "Find files from current file" true ]
+    ];
   };
 }
