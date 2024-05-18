768707c11a6a3a60c0c74b543346a8f0d629b408
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-05-19 05:24:37 +0530

    feat(gui/browser): add chromium

--- home/packages/gui/browser.nix
+++ home/packages/gui/browser.nix
@@ -3,4 +3,19 @@
   home.packages = with pkgs; [
     firefox
   ];
+
+  programs.chromium = {
+    enable = true;
+    package = pkgs.chromium.override {
+      enableWideVine = true;
+    };
+    extensions = [
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+      "nngceckbapebfimnlniiiahkandclblb" # bitwarden
+      "mjdepdfccjgcndkmemponafgioodelna" # df youtube
+      "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsor block
+      "edibdbjcniadpccecjdfdjjppcpchdlm" # i don't care about cookies
+      "jpbjcnkcffbooppibceonlgknpkniiff" # global speed
+    ];
+  };
 }
