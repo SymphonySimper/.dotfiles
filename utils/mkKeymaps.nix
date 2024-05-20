efeebf0851d942796802014106e91aaeb07574cd
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-05-20 08:58:08 +0530

    feat(editor/nvim): all keybinds are silent

--- utils/mkKeymaps.nix
+++ utils/mkKeymaps.nix
@@ -4,7 +4,10 @@ let
       action = builtins.elemAt keymap 0;
       key = builtins.elemAt keymap 1;
       mode = builtins.elemAt keymap 2;
-      options.desc = builtins.elemAt keymap 3;
+      options = {
+        desc = builtins.elemAt keymap 3;
+        silent = true;
+      };
       lua = if builtins.length keymap == 5 then builtins.elemAt keymap 4 else false;
     })
     keymaps;
