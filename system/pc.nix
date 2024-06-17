375f20e63ad6b5449f8a581e819e8d6e7e5ad3c1
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-06-17 09:43:30 +0530

    feat(system/pc): explicitly set sound to false

--- system/pc.nix
+++ system/pc.nix
@@ -8,7 +8,7 @@
   networking.networkmanager.enable = true;
 
   # Enable sound with pipewire.
-  # sound.enable = true;
+  sound.enable = false;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
