d1e52ea5feca4a293fcf7a8677465cfd947c9c14
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-07-03 15:20:27 +0530

    feat(system): add storage optimization

--- system/default.nix
+++ system/default.nix
@@ -72,4 +72,14 @@ in
       };
     };
   };
+
+  # Storage optimise
+  nix = {
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+  };
 }
