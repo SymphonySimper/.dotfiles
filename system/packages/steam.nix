d538f76aa4f2ab4a100a1c0bc04393920dd102be
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-07-03 13:35:18 +0530

    fix(system/steam): rm deprecated withJava override

--- system/packages/steam.nix
+++ system/packages/steam.nix
@@ -1,9 +1,7 @@
-{ pkgs, ... }: {
+{ ... }: {
   # Refer: https://nixos.wiki/wiki/Steam
-  programs.java.enable = true;
   programs.steam = {
     enable = true;
-    package = pkgs.steam.override { withJava = true; };
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
