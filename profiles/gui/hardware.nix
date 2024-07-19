dcc34084330232c03fd17fb327cf99290ec8162a
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-07-20 01:05:28 +0530

    fix(profiles/gui): use amd cpu

--- profiles/gui/hardware.nix
+++ profiles/gui/hardware.nix
@@ -44,5 +44,5 @@
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
