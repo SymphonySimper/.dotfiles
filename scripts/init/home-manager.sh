799691bfd7604b0b438bcc061c59e6834c5d033d
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-07-25 01:10:32 +0530

    feat: add script to instal with nixos-anywhere

--- scripts/init/home-manager.sh
+++ scripts/init/home-manager.sh
@@ -4,11 +4,17 @@ gh_name="SymphonySimper"
 gh_repo=".dotfiles"
 cd "$HOME" || exit 1
 
+profile="$1"
+if [ -z "$profile" ]; then
+  echo "No profile passed (ex: gui)."
+  exit 1
+fi
+
 echo "Cloning $gh_repo"
 if [ ! -d "$HOME/$gh_repo" ]; then
 	git clone "https://github.com/$gh_name/$gh_repo"
 fi
 
 echo "Buidling flake"
-nix build "github:$gh_name/$gh_repo#homeConfigurations.symph.activationPackage"
+nix build "github:$gh_name/$gh_repo#homeConfigurations.${profile}.activationPackage"
 result/activate
---
+++ scripts/init/nixos-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+profile="$1"
+if [ -z "$profile" ]; then
+  echo "No profile passed (ex: gui)."
+  exit 1
+fi
+
+echo "Set SSH password"
+passwd
+
+ip_addr="$(ip -brief -4 addr | tail -n1 | awk '{print $3}' | tr -d ' ' | cut -d '/' -f1)"
+echo "IP address: ${ip_addr}"
+
+nix --extra-experimental-features 'nix-command flakes' run 'github:nix-community/nixos-anywhere' -- --no-reboot --build-on-remote --flake "github:SymphonySimper/.dotfiles#${profile}" "nixos@${ip_addr}"
