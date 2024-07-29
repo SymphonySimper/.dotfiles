0bc9457d02b347ff5c4bd8afc6ccdb1d6f04aa21
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-07-29 10:00:52 +0530

    feat(system): add skip username for tty1

--- system/packages/desktop/wm/default.nix
+++ system/packages/desktop/wm/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ userSettings, pkgs, ... }:
 {
   imports = [
     # ./hyprland.nix
@@ -7,4 +7,22 @@
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.dconf.enable = true;
+
+  # Default username for all tty
+  # services.getty = {
+  #   loginOptions = "-p -- ${userSettings.username}";
+  #   extraArgs = [
+  #     "--noclear"
+  #     "--skip-login"
+  #   ];
+  # };
+
+  # Skip username only for tty1
+  systemd.services."getty@tty1" = {
+    overrideStrategy = "asDropin";
+    serviceConfig.ExecStart = [
+      ""
+      "@${pkgs.util-linux}/sbin/agetty agetty --login-program ${pkgs.shadow}/bin/login -o '-p -- ${userSettings.username}' --noclear --skip-login %I $TERM"
+    ];
+  };
 }
