5f996720287a1e78cc86c9a6fa397ec0f0589d26
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-07-29 12:06:15 +0530

    feat(home/browser): add mocha to chromium

--- home/packages/gui/browser.nix
+++ home/packages/gui/browser.nix
@@ -1,18 +1,15 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    firefox
-  ];
+  home.packages = with pkgs; [ firefox ];
 
   programs.chromium = {
     enable = true;
-    package = pkgs.chromium.override {
-      enableWideVine = true;
-    };
+    package = pkgs.chromium.override { enableWideVine = true; };
     extensions = [
       "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
       "nngceckbapebfimnlniiiahkandclblb" # bitwarden
       "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
+      "bkkmolkhemgaeaeggcmfbghljjjoofoh" # Catppuccin mocha
       # "mjdepdfccjgcndkmemponafgioodelna" # df youtube
       "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsor block
       "edibdbjcniadpccecjdfdjjppcpchdlm" # i don't care about cookies
@@ -20,8 +17,6 @@
       "bnclejfcblondkjliiblkojdeloomadd" # mute tab
       "cankofcoohmbhfpcemhmaaeennfbnmgp" # netflix 1080p
     ];
-    commandLineArgs = [
-      "--ozone-platform-hint=auto"
-    ];
+    commandLineArgs = [ "--ozone-platform-hint=auto" ];
   };
 }
