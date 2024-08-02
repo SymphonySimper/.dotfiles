06d90477b99008a0b18f3626ad47dbbb84068335
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-02 08:46:22 +0530

    feat(nixvim): update to new trouble keymaps

--- home/packages/editor/nvim/nixvim/config/plugins/trouble.nix
+++ home/packages/editor/nvim/nixvim/config/plugins/trouble.nix
@@ -11,10 +11,40 @@
 
     keymaps = myUtils.mkKeymaps [
       [
-        "<CMD>TroubleToggle workspace_diagnostics<CR>"
+        "<cmd>Trouble diagnostics toggle<cr>"
         "<leader>xx"
         "n"
-        "Workspace Diagnostics (Trouble)"
+        "Diagnostics (Trouble)"
+      ]
+      [
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>"
+        "<leader>xX"
+        "n"
+        "Buffer Diagnostics (Trouble)"
+      ]
+      [
+        "<cmd>Trouble symbols toggle focus=false<cr>"
+        "<leader>cs"
+        "n"
+        "Symbols (Trouble)"
+      ]
+      [
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>"
+        "<leader>cl"
+        "n"
+        "LSP Definitions / references / ... (Trouble)"
+      ]
+      [
+        "<cmd>Trouble loclist toggle<cr>"
+        "<leader>xL"
+        "n"
+        "Location List (Trouble)"
+      ]
+      [
+        "<cmd>Trouble qflist toggle<cr>"
+        "<leader>xQ"
+        "n"
+        "Quickfix List (Trouble)"
       ]
     ];
   };
