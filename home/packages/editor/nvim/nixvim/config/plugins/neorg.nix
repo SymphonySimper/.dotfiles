ff071cfdf8edc8d58a51a981720b7b4359809918
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-03 23:58:58 +0530

    feat(nixvim): add journal

--- home/packages/editor/nvim/nixvim/config/plugins/neorg.nix
+++ home/packages/editor/nvim/nixvim/config/plugins/neorg.nix
@@ -1,25 +1,41 @@
 { userSettings, ... }:
 {
   programs.nixvim = {
-    plugins.neorg = {
-      enable = true;
-      modules = {
-        "core.defaults" = {
-          __empty = null;
-        };
-        "core.concealer" = {
-          __empty = null;
-        };
-        "core.dirman" = {
-          config = {
-            workspaces = {
-              notes = "~/${userSettings.dirs.importantnt}/notes";
-              diary = "~/${userSettings.dirs.importantnt}/diary";
+    plugins = {
+      neorg = {
+        enable = true;
+        modules = {
+          "core.defaults" = {
+            __empty = null;
+          };
+          "core.concealer" = {
+            __empty = null;
+          };
+          "core.dirman" = {
+            config = {
+              workspaces = {
+                notes = "~/${userSettings.dirs.importantnt}/notes";
+                heart = "~/${userSettings.dirs.importantnt}/heart";
+              };
+              default_workspace = "notes";
             };
-            default_workspace = "notes";
+          };
+          "core.esupports.metagen" = {
+            config.author = "${userSettings.name.name}";
+          };
+          "core.journal" = {
+            config = {
+              journal_folder = "journal";
+              workspace = "heart";
+            };
+          };
+          "core.completion" = {
+            config.engine = "nvim-cmp";
           };
         };
       };
+
+      cmp.settings.sources = [ { name = "neorg"; } ];
     };
   };
 }
