cf1c75237c31a390dc30509c1fcca5e4b74fe890
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-03 19:47:15 +0530

    feat(nixvim): rm ensure_installed from treesitter

--- home/packages/editor/nvim/nixvim/config/plugins/treesitter.nix
+++ home/packages/editor/nvim/nixvim/config/plugins/treesitter.nix
@@ -4,34 +4,38 @@
     treesitter = {
       enable = true;
       nixvimInjections = true;
+      nixGrammars = true;
+      gccPackage = null;
+      nodejsPackage = null;
+      treesitterPackage = null;
       settings = {
         indent.enable = true;
         highlight = {
           enable = true;
           additional_vim_regex_highlighting = false;
         };
-        ensure_installed = [
-          "astro"
-          "bash"
-          "css"
-          "gitcommit"
-          "gitignore"
-          "html"
-          "javascript"
-          "jsdoc"
-          "json"
-          "lua"
-          "markdown"
-          "python"
-          "regex"
-          "scss"
-          "svelte"
-          "tsx"
-          "typescript"
-          "vim"
-          "vimdoc"
-          "yaml"
-        ];
+        # ensure_installed = [
+        #   "astro"
+        #   "bash"
+        #   "css"
+        #   "gitcommit"
+        #   "gitignore"
+        #   "html"
+        #   "javascript"
+        #   "jsdoc"
+        #   "json"
+        #   "lua"
+        #   "markdown"
+        #   "python"
+        #   "regex"
+        #   "scss"
+        #   "svelte"
+        #   "tsx"
+        #   "typescript"
+        #   "vim"
+        #   "vimdoc"
+        #   "yaml"
+        # ];
       };
     };
 
