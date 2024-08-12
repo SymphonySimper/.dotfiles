19fe0eace962c66436cfe87b7d140edd2d55729a
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-12 18:55:43 +0530

    fix(yazi): ueberzug offset

--- home/packages/cli/yazi.nix
+++ home/packages/cli/yazi.nix
@@ -9,14 +9,21 @@
     settings = {
       manager = {
         ratio = [
-          1
-          3
-          4
+          1 # left
+          3 # center
+          4 # right
         ];
       };
       preview = {
         max_height = 1920;
         max_width = 1080;
+        ueberzug_scale = 1.5;
+        ueberzug_offset = [
+          (-28.0) # x
+          (0.0) # y
+          (0.0) # w
+          (0.0) # h
+        ];
       };
     };
     plugins = {
