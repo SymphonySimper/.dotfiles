d4a310ed20a8f018a1e71aa5b589378c527b3c2b
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-13 18:28:51 +0530

    feat(flake): switch back to sway

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
           glyph = "Font Awesome 6 Free";
         };
         desktop = {
-          name = "hyprland";
+          name = "sway";
           wm = desktop.name != "gnome";
         };
         programs = {
