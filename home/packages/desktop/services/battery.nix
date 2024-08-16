e9ee6d50b750d194a96827cc871b722d3ab57eca
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-16 17:24:04 +0530

    feat(services/battery): add 2 cylce timeout when battery is below 40%

--- home/packages/desktop/services/battery.nix
+++ home/packages/desktop/services/battery.nix
@@ -10,6 +10,8 @@ in
       Type = "oneshot";
       ExecStart = pkgs.lib.getExe (
         pkgs.writeShellScriptBin "my-battery" ''
+          tmp_file='/tmp/${name}'
+
           function battery_notify() {
            ${
              myUtils.mkNotification {
@@ -19,13 +21,33 @@ in
              }
            }
           }
+
+          function default_temp_file() {
+            echo -1 > "$tmp_file";
+          }
+
           battery_status=$(</sys/class/power_supply/BAT0/status)
           battery_capacity=$(</sys/class/power_supply/BAT0/capacity)
+
           if [ $battery_capacity -gt 80 ] && [[ $battery_status == 'Charging' ]]; then
             battery_notify "Battery is greater than 80% ($battery_capacity) unplug the charger"
           fi
           if [ $battery_capacity -le 40 ] && [[ $battery_status == 'Discharging' ]]; then
-            battery_notify "Battery is less than 40% ($battery_capacity) plug the charger"
+            if [ ! -f "$tmp_file" ]; then
+              default_temp_file
+            fi
+
+            time_left=$(<"$tmp_file")
+            if [[ $time_left -eq -1 ]]; then
+              battery_notify "Battery is less than 40% ($battery_capacity) plug the charger"
+              echo 1 > "$tmp_file"
+            else
+              if [[ $time_left -eq 0 ]]; then
+                default_temp_file
+              else
+                echo $((time_left - 1)) > "$tmp_file"
+              fi
+            fi
           fi
         ''
       );
