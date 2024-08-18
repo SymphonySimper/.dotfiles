b2c3ae2a4529601bed8ffb8eabe6f7a9cd6c61ae
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-18 23:45:18 +0530

    feat(steam): add args to gamescope

--- system/packages/steam.nix
+++ system/packages/steam.nix
@@ -6,6 +6,20 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    gamescopeSession.enable = true; # gamescope -f -e --expose-wayland -W 1920 -H 1200 -- %command%
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamescope = {
+    # set launch options to gamescope -- %command%
+    args = [
+      "-f"
+      "-e"
+      "--expose-wayland"
+      "-W 1920" # 2880
+      "-H 1200" # 1800
+      "--backend wayland"
+      "-C"
+      "--force-grab-cursor"
+    ];
   };
 }
