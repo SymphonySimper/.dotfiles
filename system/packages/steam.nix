a5e17b888a9229b113e4fd363f7c93f7ea9ff094
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-18 22:57:50 +0530

    feat(steam): add gamescope launch options

--- system/packages/steam.nix
+++ system/packages/steam.nix
@@ -1,10 +1,11 @@
-{ ... }: {
+{ ... }:
+{
   # Refer: https://nixos.wiki/wiki/Steam
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    gamescopeSession.enable = true;
+    gamescopeSession.enable = true; # gamescope -f -e --expose-wayland -W 1920 -H 1200 -- %command%
   };
 }
