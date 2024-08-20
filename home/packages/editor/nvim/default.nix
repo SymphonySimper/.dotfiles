a49539b9ee4980f398db01a72936a81930b1c991
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-20 23:35:55 +0530

    feat(nvim): #6 add nix function mkPlugins to simplify plugin syntax

--- home/packages/editor/nvim/default.nix
+++ home/packages/editor/nvim/default.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
 let
-  mkPlugin = plugin: config: {
-    inherit plugin;
-    type = "lua";
-    config = if builtins.typeOf config == "path" then builtins.readFile config else config;
-  };
+  mkPlugin =
+    { plugin, config }:
+    {
+      inherit plugin;
+      type = "lua";
+      config = if builtins.typeOf config == "path" then builtins.readFile config else config;
+    };
+
+  mkPlugins =
+    plugins:
+    builtins.map (
+      plugin:
+      if builtins.typeOf plugin == "list" then
+        mkPlugin {
+          plugin = builtins.elemAt plugin 0;
+          config = builtins.elemAt plugin 1;
+        }
+      else
+        plugin
+    ) plugins;
 in
 {
   programs.neovim = {
@@ -20,48 +35,58 @@ in
         ${builtins.readFile ./config/options.lua}
       '';
 
-    plugins = with pkgs.vimPlugins; [
-      (mkPlugin nvim-harpoon ./config/plugins/harpoon.lua)
-      (mkPlugin nvim-colorizer # lua
-        "require 'colorizer'.setup()"
-      )
-      (mkPlugin nvim-lazygit # lua
-        ''
-          vim.keymap.set("n", "<leader>gz", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
-        ''
-      )
+    plugins =
+      with pkgs.vimPlugins;
+      (mkPlugins [
+        [
+          nvim-harpoon
+          ./config/plugins/harpoon.lua
+        ]
+        [
+          nvim-colorizer # lua
+          "require 'colorizer'.setup()"
+        ]
+        [
+          nvim-lazygit # lua
+          ''
+            vim.keymap.set("n", "<leader>gz", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+          ''
+        ]
 
-      # Treesitter
-      (mkPlugin (nvim-treesitter.withPlugins (p: [
-        # Treesitter grammars
-        p.tree-sitter-bash
-        p.tree-sitter-lua
-        p.tree-sitter-markdown
-        p.tree-sitter-nix
-        p.tree-sitter-python
+        # Treesitter
+        [
+          (nvim-treesitter.withPlugins (p: [
+            # Treesitter grammars
+            p.tree-sitter-bash
+            p.tree-sitter-lua
+            p.tree-sitter-markdown
+            p.tree-sitter-nix
+            p.tree-sitter-python
 
-        p.tree-sitter-html
-        p.tree-sitter-css
-        p.tree-sitter-scss
-        p.tree-sitter-javascript
-        p.tree-sitter-typescript
-        p.tree-sitter-svelte
+            p.tree-sitter-html
+            p.tree-sitter-css
+            p.tree-sitter-scss
+            p.tree-sitter-javascript
+            p.tree-sitter-typescript
+            p.tree-sitter-svelte
 
-        p.tree-sitter-dockerfile
-        p.tree-sitter-json
-        p.tree-sitter-toml
-        p.tree-sitter-yaml
+            p.tree-sitter-dockerfile
+            p.tree-sitter-json
+            p.tree-sitter-toml
+            p.tree-sitter-yaml
 
-        p.tree-sitter-gitcommit
-        p.tree-sitter-gitignore
+            p.tree-sitter-gitcommit
+            p.tree-sitter-gitignore
 
-        p.tree-sitter-vim
-        p.tree-sitter-vimdoc
-      ])) ./config/plugins/treesitter.lua)
+            p.tree-sitter-vim
+            p.tree-sitter-vimdoc
+          ]))
+          ./config/plugins/treesitter.lua
+        ]
 
-      # Common deps
-      plenary-nvim
-      nvim-web-devicons
-    ];
+        # Common deps
+        plenary-nvim
+        nvim-web-devicons
+      ]);
   };
 }
