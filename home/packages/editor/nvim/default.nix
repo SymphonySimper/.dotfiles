0d0c02eedd558fecba6de74784caa8b78ec24205
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-21 18:25:10 +0530

    refactor(nvim): simplify folder structure

--- home/packages/editor/nvim/default.nix
+++ home/packages/editor/nvim/default.nix
@@ -1,4 +1,14 @@
 { ... }:
 {
-  imports = [ ./nixvim/default.nix ];
+  imports = [ ./config/default.nix ];
+
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+
+    withNodeJs = false;
+    withRuby = false;
+  };
 }
