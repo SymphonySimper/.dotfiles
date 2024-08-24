cb7409f7ea3b15ad7e3cc26f1a4135c40f2acb4e
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-08-24 18:30:32 +0530

    feat(nvim): increase cmp entries to 8

--- home/packages/editor/nvim/config/plugins/cmp.nix
+++ home/packages/editor/nvim/config/plugins/cmp.nix
@@ -1,42 +1,42 @@
-{ ... }: {
-  programs.nixvim.plugins =
-    {
-      luasnip.enable = true;
+{ ... }:
+{
+  programs.nixvim.plugins = {
+    luasnip.enable = true;
 
-      cmp = {
-        enable = true;
-        autoEnableSources = true;
-        settings = {
-          sources = [
-            { name = "path"; }
-            { name = "nvim_lsp"; }
-            { name = "luasnip"; }
-            { name = "buffer"; }
-          ];
-          snippet.expand = ''
-            function(args)
-              require('luasnip').lsp_expand(args.body)
-            end
-          '';
-          mapping = {
-            "<C-n>" = "cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })";
-            "<C-p>" = "cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })";
-            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<C-Space>" = "cmp.mapping.complete()";
-            "<C-e>" = "cmp.mapping.abort()";
-            "<C-y>" = "cmp.mapping.confirm({ select = true })";
-          };
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        sources = [
+          { name = "path"; }
+          { name = "nvim_lsp"; }
+          { name = "luasnip"; }
+          { name = "buffer"; }
+        ];
+        snippet.expand = ''
+          function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        '';
+        mapping = {
+          "<C-n>" = "cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })";
+          "<C-p>" = "cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })";
+          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<C-y>" = "cmp.mapping.confirm({ select = true })";
+        };
 
-          performance = {
-            max_view_entries = 4;
-          };
+        performance = {
+          max_view_entries = 8;
         };
       };
-
-      cmp-path.enable = true;
-      cmp-buffer.enable = true;
-      cmp-nvim-lsp.enable = true;
-      cmp_luasnip.enable = true;
     };
+
+    cmp-path.enable = true;
+    cmp-buffer.enable = true;
+    cmp-nvim-lsp.enable = true;
+    cmp_luasnip.enable = true;
+  };
 }
