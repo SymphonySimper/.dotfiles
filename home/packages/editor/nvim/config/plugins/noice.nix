72ebc3ce8de8b09950ed93adeb4905bdc6fed65f
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-09-01 14:52:55 +0530

    fix(nvim): noice warnings

--- home/packages/editor/nvim/config/plugins/noice.nix
+++ home/packages/editor/nvim/config/plugins/noice.nix
@@ -5,6 +5,11 @@
       enable = true;
       messages.view = "mini";
       notify.view = "mini";
+      lsp.override = {
+        "vim.lsp.util.convert_input_to_markdown_lines" = true;
+        "vim.lsp.util.stylize_markdown" = true;
+        "cmp.entry.get_documentation" = true;
+      };
     };
   };
 }
