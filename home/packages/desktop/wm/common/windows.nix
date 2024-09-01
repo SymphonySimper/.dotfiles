4f045cb592f9a97b642460885d3ed4aa8bb4db99
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-09-01 20:05:22 +0530

    feat(wm): add windows files to share window workspaces

---
+++ home/packages/desktop/wm/common/windows.nix
@@ -0,0 +1,51 @@
+let
+  mkChromiumAppId = url: "chrome-${url}__-Default";
+in
+{
+  "1" = {
+    app_id = [
+      "Alacritty"
+      "footclient"
+      "foot"
+      "org.wezfurlong.wezterm"
+    ];
+  };
+  "2" = {
+    app_id = [
+      "firefox"
+      "chromium-browser"
+      "Brave-browser"
+    ];
+  };
+  "3" = {
+    app_id = [
+      "com.github.johnfactotum.Foliate"
+      "org.pwmt.zathura"
+    ];
+  };
+  "4" = {
+    app_id = [
+      "mpv"
+      (mkChromiumAppId "monkeytype.com")
+    ];
+  };
+  "5" = {
+    class = [
+      "steam"
+      "thunderbird"
+    ];
+  };
+  "6" = {
+    app_id = [
+      "gamescope"
+      "discord"
+    ];
+  };
+  "9" = {
+    app_id = [ "org.qbittorrent.qBittorrent" ];
+  };
+  "0" = {
+    app_id = [ "" ];
+    title = [ "meet.google.com is sharing your screen." ];
+  };
+}
