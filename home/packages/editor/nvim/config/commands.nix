e0fa00e0f4a5b829dbe65674e77174d2d9890f70
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-10-18 23:01:54 +0530

    feat(nvim/commands): add MySratch command and keybind to open sratch window in vsplit

--- home/packages/editor/nvim/config/commands.nix
+++ home/packages/editor/nvim/config/commands.nix
@@ -1,22 +1,48 @@
-{ ... }:
+{ lib, ... }:
 {
-  programs.nixvim.userCommands = {
-    # show :messages in a scratch buffer
-    "MyMessages" = {
-      command = {
-        __raw = ''
-          function ()
-            local window = vim.api.nvim_get_current_win()
-            local buffer = vim.api.nvim_create_buf(true, true)
-            vim.api.nvim_open_win(buffer, true, {
-              split = "right",
-              win = window
-            })
-            local messages = vim.split( vim.fn.execute("messages"), "\n" )
-            vim.api.nvim_buf_set_lines(buffer, 0, -1, false, messages)
-          end
-        '';
+  programs.nixvim = {
+    userCommands = {
+      # show :messages in a scratch buffer
+      "MyMessages" = {
+        command = {
+          __raw = ''
+            function ()
+              local window = vim.api.nvim_get_current_win()
+              local buffer = vim.api.nvim_create_buf(true, true)
+              vim.api.nvim_open_win(buffer, true, {
+                split = "right",
+                win = window
+              })
+              local messages = vim.split( vim.fn.execute("messages"), "\n" )
+              vim.api.nvim_buf_set_lines(buffer, 0, -1, false, messages)
+            end
+          '';
+        };
+      };
+      # open a sratch window
+      "MySratch" = {
+        command = {
+          __raw = ''
+            function ()
+              local window = vim.api.nvim_get_current_win()
+              local buffer = vim.api.nvim_create_buf(true, true)
+              vim.api.nvim_open_win(buffer, true, {
+                split = "right",
+                win = window
+              })
+            end
+          '';
+        };
       };
     };
+
+    keymaps = lib.my.mkKeymaps [
+      [
+        "<CMD>MySratch<CR>"
+        "<leader>fs"
+        "n"
+        "Open a sratch in vsplit"
+      ]
+    ];
   };
 }
