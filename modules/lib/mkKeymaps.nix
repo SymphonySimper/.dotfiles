f100126f5aa90fa63c562fa947720d30c4218d62
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-10-18 21:47:59 +0530

    feat(nvim)!: disable keymap silent

--- modules/lib/mkKeymaps.nix
+++ modules/lib/mkKeymaps.nix
@@ -1,14 +1,14 @@
 let
-  mkKeymap = keymaps: map
-    (keymap: {
+  mkKeymap =
+    keymaps:
+    map (keymap: {
       action = builtins.elemAt keymap 0;
       key = builtins.elemAt keymap 1;
       mode = builtins.elemAt keymap 2;
       options = {
         desc = builtins.elemAt keymap 3;
-        silent = true;
+        silent = false;
       };
-    })
-    keymaps;
+    }) keymaps;
 in
 mkKeymap
