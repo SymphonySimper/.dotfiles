8efaeb8e7a59c65568ad749ba0a48804de36f215
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-11-16 01:51:49 +0530

    feat(tmux)!: rm session status from status bar

--- home/packages/cli/tmux.nix
+++ home/packages/cli/tmux.nix
@@ -37,7 +37,6 @@ in
         set -g status-left "#{E:@catppuccin_pane_current_path}"
         set -g status-right "#{E:@catppuccin_status_application}"
         set -g status-right "#{E:@catppuccin_status_application}"
-        set -ag status-right "#{E:@catppuccin_status_session}"
       '';
     extraConfig = # tmux
       ''
