9cc6d371f5098ca007b5ea3ce7b8e9277de65eaa
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-11-24 20:41:55 +0530

    feat(nvim/cmp)!: replace nvim-cmp with blink.cmp

--- modules/home/cli/nvim/config/plugins/cmp.nix
+++ modules/home/cli/nvim/config/plugins/cmp.nix
@@ -1,31 +1,13 @@
 { ... }:
 {
   programs.nixvim.plugins = {
-    # cmp
-    cmp-nvim-lsp.enable = true;
-    cmp-path.enable = true;
-    cmp = {
+    friendly-snippets.enable = true;
+    blink-cmp = {
       enable = true;
-      autoEnableSources = true;
       settings = {
-        preselect = "cmp.PreselectMode.None";
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-        ];
-        mapping = {
-          "<C-n>" = "cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })";
-          "<C-p>" = "cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<C-y>" = "cmp.mapping.confirm({ select = true })";
-        };
-
-        performance = {
-          max_view_entries = 8;
-        };
+        nerd_font_variant = "mono";
+        keymap.preset = "default";
+        fuzzy.max_items = 20;
       };
     };
   };
