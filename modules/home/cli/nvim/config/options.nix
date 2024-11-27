196426a0077a62da193100285071f7ec416d078f
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-11-27 23:16:05 +0530

    feat(nvim/options): better wrap and cmd, status settings

--- modules/home/cli/nvim/config/options.nix
+++ modules/home/cli/nvim/config/options.nix
@@ -1,35 +1,49 @@
 { ... }:
 {
   programs.nixvim.opts = {
-    # Number line
+    # line
+    ## number
     number = true;
     relativenumber = true;
-
-    # Scroll
+    ## wrap
+    wrap = true;
+    linebreak = true;
+    breakindent = true;
+    ## scroll
     scrolloff = 8; # Vertical scroll
     sidescrolloff = 8; # Horizontal scroll
-
-    undofile = false; # Turn off undofile
-
+    ## misc
+    smartindent = true;
     foldmethod = "manual"; # Create folds with visual selection
 
-    # UI
+    # column
     signcolumn = "yes";
-    wrap = false;
 
-    clipboard = "";
+    # status line & cmd
+    ruler = false; # do not show cursor position
+    showcmd = false; # do not show key pressed on cmd
 
     # tab settings
     tabstop = 2;
     shiftwidth = 2;
+    shiftround = true;
     expandtab = true;
 
+    # split
     splitbelow = true;
     splitright = true;
 
+    # serach
     ignorecase = true;
     smartcase = true;
-    smartindent = true;
+    inccommand = "split"; # preview for `%s/foo/bar/g`
+
+    # file
+    clipboard = "";
+    undofile = false; # Turn off undofile
+    confirm = true; # prompt to save changes
+
+    # misc
     termguicolors = true;
   };
 }
