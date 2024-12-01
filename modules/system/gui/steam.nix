3a967870b6adf10a0b51e1251469e0c5d2be755c
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-12-01 20:00:22 +0530

    feat(steam)!: install outside of specialisation

--- modules/system/gui/steam.nix
+++ modules/system/gui/steam.nix
@@ -48,6 +48,28 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
+    # Refer: https://nixos.wiki/wiki/Steam
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      gamescopeSession = {
+        enable = true;
+        # inherit args;
+        # env = {
+        #   LD_PRELOAD = "";
+        # };
+      };
+    };
+
+    programs.gamescope = {
+      enable = true;
+      capSysNice = false;
+      # set launch options to `LD_PRELOAD="" gamescope -- %command%`
+      inherit args;
+    };
+
     specialisation = {
       steam = {
         inheritParentConfig = true;
@@ -55,28 +77,6 @@ in
           my = {
             programs.wm.enableLogin = false;
             hardware.powerManagement.enable = false;
-          };
-
-          # Refer: https://nixos.wiki/wiki/Steam
-          programs.steam = {
-            enable = true;
-            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-            dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-            localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-            gamescopeSession = {
-              enable = true;
-              # inherit args;
-              # env = {
-              #   LD_PRELOAD = "";
-              # };
-            };
-          };
-
-          programs.gamescope = {
-            enable = true;
-            capSysNice = false;
-            # set launch options to `LD_PRELOAD="" gamescope -- %command%`
-            inherit args;
           };
 
           environment = {
