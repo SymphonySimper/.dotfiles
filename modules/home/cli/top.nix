ffcbb83affb710f6d72b7e403063ae986f4346a9
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-12-18 18:36:15 +0530

    feat(cli/top): add htop column settings

--- modules/home/cli/top.nix
+++ modules/home/cli/top.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, config, ... }:
 {
   programs = {
     btop = {
@@ -8,18 +8,39 @@
         background_update = false;
       };
     };
+
     htop = {
       enable = true;
-      settings = {
-        color_scheme = 0;
+      settings = lib.mkMerge [
+        {
+          color_scheme = 0;
 
-        tree_view = 1;
-        all_branches_collapsed = 1;
-        show_program_path = 0;
-        highlight_base_name = 1;
+          tree_view = 1;
+          all_branches_collapsed = 1;
+          show_program_path = 0;
+          highlight_base_name = 1;
 
-        hide_kernel_threads = 1;
-      };
+          hide_kernel_threads = 1;
+        }
+        (
+          with config.lib.htop;
+          leftMeters [
+            (bar "CPU")
+            (bar "LeftCPUs2")
+            (bar "Memory")
+            (bar "Swap")
+          ]
+        )
+        (
+          with config.lib.htop;
+          rightMeters [
+            (bar "RightCPUs2")
+            (text "Tasks")
+            (text "LoadAverage")
+            (text "Uptime")
+          ]
+        )
+      ];
     };
   };
 }
