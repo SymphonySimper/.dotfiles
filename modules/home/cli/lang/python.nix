f76b7c3b703b05b0f874f9f54b6778a77bcd517f
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-01-08 14:00:06 +0530

    feat(lang/python)!: rm micromamba

--- modules/home/cli/lang/python.nix
+++ modules/home/cli/lang/python.nix
@@ -1,58 +1,23 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   home = {
     packages = with pkgs; [
       python3
-      micromamba
     ];
 
     shellAliases = {
       py = "python";
-      pc = "micromamba";
-      pca = "micromamba activate";
-      pcd = "micromamba deactivate";
-      pcce = "micromamba create -n";
-      pcrm = "micromamba env remove -n";
+      pvc = "python -m venv .venv";
+      pva = "source .venv/bin/activate";
       pfrd = "flask run --debug";
     };
-    sessionVariables = {
-      # CONDARCA = "${config.xdg.configHome}/conda/condarc";
-      MAMBA_ROOT_PREFIX = "${config.xdg.dataHome}/mamba";
+  };
+
+  programs.poetry = {
+    enable = true;
+    settings.virtualenvs = {
+      create = true;
+      in-project = true;
     };
   };
-
-  programs = {
-    poetry = {
-      enable = true;
-      settings.virtualenvs = {
-        create = true;
-        in-project = true;
-      };
-    };
-    zsh.initExtra = # sh
-      ''
-        eval "$(micromamba shell hook -s zsh)"
-      '';
-    bash.initExtra = # sh
-      ''
-        eval "$(micromamba shell hook -s bash)"
-      '';
-  };
-
-  xdg.configFile."mamba/mambarc" = {
-    text = ''
-      channels:
-        - conda-forge
-      always_yes: false
-      auto_activate_base: false
-      changeps1: false
-    '';
-  };
-
-  # xdg.configFile."conda/condarc" = {
-  #   text = ''
-  #     auto_activate_base: false
-  #     changeps1: false
-  #   '';
-  # };
 }
