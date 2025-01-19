0171550afe86b9fe48c97cd353f2b92c10bb3223
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-01-19 14:23:35 +0530

    feat(nvim/autocmds): simplify adding new commands

--- modules/home/cli/nvim/config/autocmds.nix
+++ modules/home/cli/nvim/config/autocmds.nix
@@ -1,26 +1,54 @@
 { ... }:
+let
+  commands = [
+    {
+      # Highlight when yanking (copying) text
+      event = "TextYankPost";
+      group = "my-highlight-yank";
+      callback = {
+        __raw =
+          # lua
+          ''
+            function()
+              vim.highlight.on_yank({higroup="Visual", timeout=200})
+            end
+          '';
+      };
+    }
+  ];
+
+  processedCommands =
+    builtins.mapAttrs (_: values: (builtins.map (value: builtins.removeAttrs value [ "type" ]) values))
+      (
+        builtins.groupBy (command: command.type) (
+          builtins.concatMap (
+            command:
+            let
+              group = command.group;
+            in
+            [
+              {
+                type = "group";
+                name = group;
+                value = {
+                  clear = true;
+                };
+              }
+
+              (
+                {
+                  type = "command";
+                }
+                // command
+              )
+            ]
+          ) commands
+        )
+      );
+in
 {
   programs.nixvim = {
-    autoGroups = {
-      "my-highlight-yank" = {
-        clear = true;
-      };
-    };
-    autoCmd = [
-      {
-        # Highlight when yanking (copying) text
-        event = "TextYankPost";
-        group = "my-highlight-yank";
-        callback = {
-          __raw =
-            # lua
-            ''
-              function()
-                vim.highlight.on_yank({higroup="Visual", timeout=200})
-              end
-            '';
-        };
-      }
-    ];
+    autoGroups = builtins.listToAttrs (processedCommands.group);
+    autoCmd = processedCommands.command;
   };
 }
