a1fafb727cab5cdf04d2dc017be09b51ef6ecb54
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-01-20 19:56:56 +0530

    feat(gpu/amd)!: rm extra packages

--- modules/system/hardware/gpu/amd.nix
+++ modules/system/hardware/gpu/amd.nix
@@ -1,25 +1,18 @@
 {
-  pkgs,
-  lib,
   config,
+  lib,
   ...
 }:
 {
   options.my.hardware.gpu.amd = {
     enable = lib.mkEnableOption "AMD GPU";
   };
+
   config = lib.mkIf config.my.hardware.gpu.amd.enable {
     hardware = {
       graphics = {
         enable = true;
         enable32Bit = true;
-        extraPackages = with pkgs; [
-          vaapiVdpau
-          libvdpau-va-gl
-          vulkan-loader
-          vulkan-validation-layers
-          vulkan-extension-layer
-        ];
       };
 
       amdgpu.initrd.enable = true;
