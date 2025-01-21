5e63dd64c9641f0e5e905fb512b43957411c4688
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-01-21 12:08:44 +0530

    feat(system/user): add option to add groups

--- modules/system/user.nix
+++ modules/system/user.nix
@@ -1,23 +1,40 @@
-{ pkgs, my, ... }:
 {
-  programs.zsh.enable = true;
-  users = {
-    mutableUsers = true;
-    defaultUserShell = pkgs.zsh;
-    users.${my.name} = {
-      initialHashedPassword = "$6$zzXPOtlNAnpUTgHe$.VZIkoqeZQWtACW6JFOZBeUUT5ds7PDpfoMZQOfWNCND0ukdGVd7jA2Ko86g8tPDxfpM3D0rVkCRUfEz/hJiN0";
-      isNormalUser = true;
-      home = my.dir.home;
-      description = "${my.fullName}";
-      extraGroups = [
-        "wheel"
-        "networkmanager"
-        "uinput"
-        "libvirtd"
-        "video"
-        "input"
-        "disk"
-      ];
+  my,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  options.my.user.groups = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    description = "Adds groups to extraGroups";
+    default = [ ];
+  };
+
+  config = {
+    programs.zsh.enable = true;
+    users = {
+      mutableUsers = true;
+      defaultUserShell = pkgs.zsh;
+      users.${my.name} = {
+        initialHashedPassword = "$6$zzXPOtlNAnpUTgHe$.VZIkoqeZQWtACW6JFOZBeUUT5ds7PDpfoMZQOfWNCND0ukdGVd7jA2Ko86g8tPDxfpM3D0rVkCRUfEz/hJiN0";
+        isNormalUser = true;
+        home = my.dir.home;
+        description = "${my.fullName}";
+        extraGroups = lib.lists.unique (
+          [
+            "wheel"
+            "networkmanager"
+            "uinput"
+            "libvirtd"
+            "video"
+            "input"
+            "disk"
+          ]
+          ++ config.my.user.groups
+        );
+      };
     };
   };
 }
