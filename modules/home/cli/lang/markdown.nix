cb466d582b89444b3b33cffac6da7b7dc26deea0
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-01-27 21:36:19 +0530

    feat(lang/markdown)!: rm markview

--- modules/home/cli/lang/markdown.nix
+++ modules/home/cli/lang/markdown.nix
@@ -9,31 +9,5 @@
       packages = [ pkgs.nodePackages.prettier ];
       ft.markdown = "prettier";
     };
-
-    markview = {
-      enable = true;
-      settings = {
-        modes = [
-          "n"
-          "no"
-          "c"
-        ];
-
-        list_items = {
-          shift_width = 2;
-        };
-
-        hybrid_modes = [ "n" ];
-        callbacks = {
-          on_enable.__raw = # lua
-            ''
-              function (_, win)
-                 vim.wo[win].conceallevel = 2;
-                 vim.wo[win].concealcursor = "c";
-               end
-            '';
-        };
-      };
-    };
   };
 }
