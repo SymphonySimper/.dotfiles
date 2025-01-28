7660100c8c6e8eff00c7cb8ce55f10cf9c7cfd40
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-01-28 17:08:41 +0530

    fix(wm/screenshot): use grimblast

--- modules/home/gui/desktop/wm/scripts/screenshot.nix
+++ modules/home/gui/desktop/wm/scripts/screenshot.nix
@@ -4,14 +4,14 @@
     (pkgs.writeShellScriptBin "screenshot" ''
       take_screenshot() {
         current_date=$(date +%s)
-        ${lib.getExe pkgs.sway-contrib.grimshot} --notify --cursor \
-        savecopy $1 \
+        ${lib.getExe pkgs.grimblast} --notify --cursor \
+        copysave $1 \
         "$HOME/Pictures/Screenshots/$current_date.png"
       }
 
       case "$1" in
       -r) take_screenshot area ;;
-      -w) take_screenshot window ;;
+      -w) take_screenshot active ;;
       -s) take_screenshot output ;;
       esac
     '')
