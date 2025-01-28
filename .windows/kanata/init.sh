93f61c5546afdd3653853faefbb3f8445450c6b6
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-01-28 09:00:10 +0530

    feat(.windows/kanata): add script to setup

---
+++ .windows/kanata/init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cp -vr ~/.dotfiles/.windows/kanata /mnt/c/Users/symph/Documents/
+cp -v ./kanata-run-shortcut.lnk "/mnt/c/Users/symph/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/"
+cp -v ./kanata-run-shortcut.lnk "/mnt/c/Users/symph/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/"
