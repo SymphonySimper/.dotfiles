8e8c2f8f403d81dc4eeff6d2a78e7948608893aa
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-01-29 20:15:22 +0530

    feat(scripts/toggle-fps): set vrr to tru for max regresh rate

--- modules/home/gui/desktop/wm/scripts/toggle-fps.nix
+++ modules/home/gui/desktop/wm/scripts/toggle-fps.nix
@@ -9,9 +9,9 @@ let
   jq = "${pkgs.jq}/bin/jq";
 
   mkMonitor =
-    refreshRate: # sh
+    refreshRate: vrr: # sh
     ''
-      ${hyprctl} keyword monitor '${my.gui.display.name}, ${my.gui.display.string.width}x${my.gui.display.string.height}@${refreshRate}Hz, auto, ${my.gui.display.string.scale}'
+      ${hyprctl} keyword monitor '${my.gui.display.name}, ${my.gui.display.string.width}x${my.gui.display.string.height}@${refreshRate}Hz, auto, ${my.gui.display.string.scale}, vrr, ${if vrr then "2" else "0"}'
     '';
 in
 {
@@ -24,10 +24,10 @@ in
       function toggle() {
         rr_set=${my.gui.display.string.maxRefreshRate}
         if [[ $(get_rr) -eq ${my.gui.display.string.maxRefreshRate} ]]; then
-          ${mkMonitor my.gui.display.string.refreshRate}
+          ${mkMonitor my.gui.display.string.refreshRate false}
           rr_set=${my.gui.display.string.refreshRate}
         else
-          ${mkMonitor my.gui.display.string.maxRefreshRate}
+          ${mkMonitor my.gui.display.string.maxRefreshRate true}
         fi
 
         ${lib.my.mkNotification { title = "RefreshRate is set to \${rr_set}Hz"; }}
