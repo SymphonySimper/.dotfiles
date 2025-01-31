0cc1f00465e2f207961a4e952214d999e737472a
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-02-01 02:00:23 +0530

    feat(cli/top)!: rm btop

--- modules/home/cli/top.nix
+++ modules/home/cli/top.nix
@@ -1,46 +1,36 @@
 { lib, config, ... }:
 {
-  programs = {
-    btop = {
-      enable = true;
-      settings = {
-        vim_keys = true;
-        background_update = false;
-      };
-    };
+  programs.htop = {
+    enable = true;
+    settings = lib.mkMerge [
+      {
+        color_scheme = 0;
 
-    htop = {
-      enable = true;
-      settings = lib.mkMerge [
-        {
-          color_scheme = 0;
+        tree_view = 1;
+        all_branches_collapsed = 1;
+        show_program_path = 0;
+        highlight_base_name = 1;
 
-          tree_view = 1;
-          all_branches_collapsed = 1;
-          show_program_path = 0;
-          highlight_base_name = 1;
-
-          hide_kernel_threads = 1;
-        }
-        (
-          with config.lib.htop;
-          leftMeters [
-            (bar "CPU")
-            (bar "LeftCPUs2")
-            (bar "Memory")
-            (bar "Swap")
-          ]
-        )
-        (
-          with config.lib.htop;
-          rightMeters [
-            (bar "RightCPUs2")
-            (text "Tasks")
-            (text "LoadAverage")
-            (text "Uptime")
-          ]
-        )
-      ];
-    };
+        hide_kernel_threads = 1;
+      }
+      (
+        with config.lib.htop;
+        leftMeters [
+          (bar "CPU")
+          (bar "LeftCPUs2")
+          (bar "Memory")
+          (bar "Swap")
+        ]
+      )
+      (
+        with config.lib.htop;
+        rightMeters [
+          (bar "RightCPUs2")
+          (text "Tasks")
+          (text "LoadAverage")
+          (text "Uptime")
+        ]
+      )
+    ];
   };
 }
