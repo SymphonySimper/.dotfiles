c0b000af18bf6ac081f1ac5f44d906ee864d9fa3
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-02-01 03:04:51 +0530

    feat(desktop/wm)!: rm udiskie

--- modules/home/gui/desktop/wm/default.nix
+++ modules/home/gui/desktop/wm/default.nix
@@ -32,16 +32,13 @@ in
     ./xdg.nix
   ];
 
-  services = {
-    udiskie.enable = true;
-    gnome-keyring = {
-      enable = true;
-      components = [
-        "pkcs11"
-        "secrets"
-        "ssh"
-      ];
-    };
+  services.gnome-keyring = {
+    enable = true;
+    components = [
+      "pkcs11"
+      "secrets"
+      "ssh"
+    ];
   };
 
   home.packages = with pkgs; [
