acb8bf4ebdab6a31da3ac262f101a90e614e12f5
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-02-02 18:05:55 +0530

    feat(system/gaming)!: disable *.openFirewall

--- modules/system/gui/gaming.nix
+++ modules/system/gui/gaming.nix
@@ -47,9 +47,10 @@ in
         enable = true;
         # Refer: https://nixos.wiki/wiki/Steam
         extest.enable = true;
-        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+
+        dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+        localNetworkGameTransfers.openFirewall = false; # Open ports in the firewall for Steam Local Network Game Transfers
+        remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
 
         gamescopeSession.enable = false;
       };
