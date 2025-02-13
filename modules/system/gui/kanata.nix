e46f5d95d492a16880bb931b3cfbde30acb7d476
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-02-13 09:15:26 +0530

    feat(gui/kanata): reload without password

--- modules/system/gui/kanata.nix
+++ modules/system/gui/kanata.nix
@@ -1,6 +1,10 @@
 { lib, my, ... }:
 {
   config = lib.mkIf my.gui.desktop.enable {
+    my.user.sudo.nopasswd = [
+      "/run/current-system/sw/bin/systemctl restart kanata-keyboard.service"
+    ];
+
     services.kanata = {
       enable = true;
       keyboards.keyboard = {
