39421743f17c3bb0424399351aa966eeb5222689
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-02-15 14:14:43 +0530

    feat(cli/helix): add script to delete and build grammars

--- modules/home/cli/helix.nix
+++ modules/home/cli/helix.nix
@@ -2,6 +2,7 @@
   my,
   inputs,
   config,
+  pkgs,
   lib,
   ...
 }:
@@ -26,76 +27,92 @@
     };
   };
 
-  config.programs.helix = {
-    enable = true;
-    package = inputs.helix.packages.${my.system}.default;
-    defaultEditor = true;
+  config = {
+    home.packages = [
+      (pkgs.writeShellScriptBin "myhelix" # sh
+        ''
+          case "$1" in
+            gb)
+              rm -rf ~/.config/helix/runtime/grammars
+              hx -g fetch
+              hx -g build
+            ;;
+          esac
+        ''
+      )
+    ];
 
-    settings = {
-      editor = {
-        line-number = "relative";
-        auto-format = false;
-        bufferline = "never";
-        auto-pairs = true;
-        true-color = my.profile == "wsl";
-        indent-guides.render = false;
+    programs.helix = {
+      enable = true;
+      package = inputs.helix.packages.${my.system}.default;
+      defaultEditor = true;
 
-        cursor-shape = rec {
-          normal = "block";
-          insert = "bar";
-          select = normal;
+      settings = {
+        editor = {
+          line-number = "relative";
+          auto-format = false;
+          bufferline = "never";
+          auto-pairs = true;
+          true-color = my.profile == "wsl";
+          indent-guides.render = false;
+
+          cursor-shape = rec {
+            normal = "block";
+            insert = "bar";
+            select = normal;
+          };
+
+          end-of-line-diagnostics = "hint";
+          inline-diagnostics.cursor-line = "warning";
+          lsp = {
+            enable = true;
+            display-inlay-hints = false;
+          };
         };
 
-        end-of-line-diagnostics = "hint";
-        inline-diagnostics.cursor-line = "warning";
-        lsp = {
-          enable = true;
-          display-inlay-hints = false;
+        keys.normal = {
+          space = {
+            b = {
+              c = ":bc";
+              w = ":w";
+              r = ":reload";
+              R = ":reload-all";
+            };
+
+            c = {
+              c = "toggle_comments";
+              C = "toggle_block_comments";
+              f = ":format";
+              a = "code_action";
+              r = "rename_symbol";
+              l = ":lsp-restart";
+              h = "select_references_to_symbol_under_cursor";
+            };
+
+            f = {
+              "'" = "last_picker";
+              f = "file_picker";
+              F = "file_picker_in_current_buffer_directory"; # prev: file_picker_in_current_directory
+              b = "buffer_picker";
+              j = "jumplist_picker";
+              g = "changed_file_picker";
+              s = "symbol_picker";
+              S = "workspace_symbol_picker";
+              d = "diagnostics_picker";
+              D = "workspace_diagnostics_picker";
+              "/" = "global_search";
+            };
+
+            q = ":quit";
+          };
         };
       };
 
-      keys.normal = {
-        space = {
-          b = {
-            c = ":bc";
-            w = ":w";
-            r = ":reload";
-            R = ":reload-all";
-          };
-
-          c = {
-            c = "toggle_comments";
-            C = "toggle_block_comments";
-            f = ":format";
-            a = "code_action";
-            r = "rename_symbol";
-            l = ":lsp-restart";
-            h = "select_references_to_symbol_under_cursor";
-          };
-
-          f = {
-            "'" = "last_picker";
-            f = "file_picker";
-            F = "file_picker_in_current_buffer_directory"; # prev: file_picker_in_current_directory
-            b = "buffer_picker";
-            j = "jumplist_picker";
-            g = "changed_file_picker";
-            s = "symbol_picker";
-            S = "workspace_symbol_picker";
-            d = "diagnostics_picker";
-            D = "workspace_diagnostics_picker";
-            "/" = "global_search";
-          };
-
-          q = ":quit";
-        };
+      languages = {
+        use-grammars.only = config.programs.helix.grammars;
+        language-server = config.programs.helix.lsp;
+        language = config.programs.helix.language;
       };
-    };
-
-    languages = {
-      use-grammars.only = config.programs.helix.grammars;
-      language-server = config.programs.helix.lsp;
-      language = config.programs.helix.language;
     };
   };
 }
