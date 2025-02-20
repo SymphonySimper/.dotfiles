502231ad249219f9f1cd8b2dad55cd53dd5c2e2f
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-02-20 19:47:59 +0530

    feat(desktop/services)!: auto import

--- modules/home/gui/desktop/services/default.nix
+++ modules/home/gui/desktop/services/default.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ lib, ... }:
 {
-  imports = [
-    ./battery.nix
-    ./rest.nix
-  ];
+  imports = builtins.map (file: ./. + "/${file}") (
+    builtins.filter (file: file != "default.nix") (
+      lib.my.mkReadDir {
+        path = ./.;
+        filter = "file";
+      }
+    )
+  );
 }
--- modules/home/gui/desktop/services/rest.nix
+++ modules/home/gui/desktop/services/rest.nix
@@ -1,49 +1,40 @@
+{ pkgs, lib, ... }:
 {
-  my,
-  pkgs,
-  lib,
-  ...
-}:
-{
-  config = lib.mkIf my.gui.desktop.enable (
-    lib.my.mkSystemdTimer rec {
-      name = "my-rest";
-      time = "15m";
-      desc = "Rest timer";
-      ExecStart = lib.getExe (
-        pkgs.writeShellScriptBin "my-rest" ''
-          tmp_file='/tmp/${name}'
+  config = lib.my.mkSystemdTimer rec {
+    name = "my-rest";
+    time = "15m";
+    desc = "Rest timer";
+    ExecStart = lib.getExe (
+      pkgs.writeShellScriptBin "my-rest" ''
+        tmp_file='/tmp/${name}'
 
-          function notify() {
-           ${
-             lib.my.mkNotification {
-               tag = "my-rest-status";
-               title = "$1";
-               body= "$2";
-               urgency = "critical";
-             }
-           }
-          }
+        function notify() {
+         ${lib.my.mkNotification {
+           tag = "my-rest-status";
+           title = "$1";
+           body = "$2";
+           urgency = "critical";
+         }}
+        }
 
-          if [ ! -f "$tmp_file" ]; then
-            echo 0 > "$tmp_file"
-          else
-            curr_count=$(<$tmp_file)
-            new_count=$((curr_count + 1))
-            title="Short rest"
-            body="15 minutes!"
+        if [ ! -f "$tmp_file" ]; then
+          echo 0 > "$tmp_file"
+        else
+          curr_count=$(<$tmp_file)
+          new_count=$((curr_count + 1))
+          title="Short rest"
+          body="15 minutes!"
 
-            if [ $new_count -eq 5 ]; then
-              new_count=1
-              title="Long rest"
-              body="60 minutes!"
-            fi
-            echo $new_count > "$tmp_file"
-
-            notify "$title" "$body"
+          if [ $new_count -eq 5 ]; then
+            new_count=1
+            title="Long rest"
+            body="60 minutes!"
           fi
-        ''
-      );
-    }
-  );
+          echo $new_count > "$tmp_file"
+
+          notify "$title" "$body"
+        fi
+      ''
+    );
+  };
 }
