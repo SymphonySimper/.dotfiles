316e2978c15ae9925fa5f34540a0082162c2c678
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-03-11 09:40:49 +0530

    feat(scripts/screenshot): rm --cursor and add --freeze

--- modules/home/gui/desktop/scripts/screenshot.nix
+++ modules/home/gui/desktop/scripts/screenshot.nix
@@ -3,14 +3,13 @@
 ''
   take_screenshot() {
     current_date=$(date +%s)
-    ${lib.getExe pkgs.grimblast} --notify --cursor \
-    copysave $1 \
+    ${lib.getExe pkgs.grimblast} --notify --freeze copysave $1 \
     "$HOME/Pictures/Screenshots/$current_date.png"
   }
 
   case "$1" in
   -r) take_screenshot area ;;
   -w) take_screenshot active ;;
-  -s) take_screenshot output ;;
+  -s) take_screenshot screen ;;
   esac
 ''
