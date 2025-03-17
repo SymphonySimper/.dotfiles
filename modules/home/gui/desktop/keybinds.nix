b915287dcc18dd37b628763cdb45fb0b1aa58b45
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-03-17 08:18:49 +0530

    feat(desktop/keybinds): better format

--- modules/home/gui/desktop/keybinds.nix
+++ modules/home/gui/desktop/keybinds.nix
@@ -9,112 +9,108 @@ let
     file: "${lib.getExe pkgs.dex} ${my.dir.home}/.nix-profile/share/applications/${file}.desktop";
 
   keybinds = {
-    terminal = {
-      default = {
-        key = "Return";
-        cmd = my.programs.terminal;
-      };
+    terminal.default = {
+      key = "Return";
+      cmd = my.programs.terminal;
     };
-    notifybar = {
-      default = {
-        key = "b";
-        cmd = "mynotifybar";
-      };
+
+    notifybar.default = {
+      key = "b";
+      cmd = "mynotifybar";
     };
-    launcher = {
-      default = {
-        key = "d";
-        cmd = "uwsm app -- $(${my.programs.launcher} --show drun --define=drun-print_desktop_file=true)";
-      };
+
+    launcher.default = {
+      key = "d";
+      cmd = "uwsm app -- $(${my.programs.launcher} --show drun --define=drun-print_desktop_file=true)";
     };
-    files = {
-      default = {
-        key = "e";
-        cmd = "uwsm app -- ${lib.getExe pkgs.nautilus}";
-      };
+
+    files.default = {
+      key = "e";
+      cmd = "uwsm app -- ${lib.getExe pkgs.nautilus}";
     };
-    browser = {
-      default = {
-        key = "f";
-        cmd = my.programs.browser;
-      };
+
+    browser.default = {
+      key = "f";
+      cmd = my.programs.browser;
     };
-    power = {
-      off = {
-        mod = "Shift";
-        key = "x";
-        cmd = "poweroff";
-      };
+
+    power.off = {
+      mod = "Shift";
+      key = "x";
+      cmd = "poweroff";
     };
+
     volume = {
       down = {
         key = "F2";
         cmd = "myvolume -d";
       };
+
       up = {
         key = "F3";
         cmd = "myvolume -u";
       };
+
       toggle = {
         mod = "Shift";
         key = "F2";
         cmd = "myvolume -m";
       };
     };
-    network = {
-      reload = {
-        mod = "Shift";
-        key = "F5";
-        cmd = "myreload";
-      };
+
+    reload.default = {
+      mod = "Shift";
+      key = "F5";
+      cmd = "myreload";
     };
+
     brightness = {
       down = {
         key = "F5";
         cmd = "mybrightness -d";
       };
+
       up = {
         key = "F6";
         cmd = "mybrightness -u";
       };
     };
-    music = {
-      open = {
-        key = "F7";
-        super = false;
-        cmd = (mkOpenDesktopEntry "ytmusic");
-      };
+
+    music.default = {
+      key = "F7";
+      super = false;
+      cmd = (mkOpenDesktopEntry "ytmusic");
     };
-    mic = {
-      toggle = {
-        super = false;
-        key = "F8";
-        cmd = "myvolume -M";
-      };
+
+    mic.toggle = {
+      super = false;
+      key = "F8";
+      cmd = "myvolume -M";
     };
-    caffiene = {
-      toggle = {
-        key = "F10";
-        cmd = "mycaffiene";
-      };
+
+    caffiene.toggle = {
+      key = "F10";
+      cmd = "mycaffiene";
     };
-    maxFps = {
-      toggle = {
-        mod = "Shift";
-        key = "F10";
-        cmd = "mytogglefps";
-      };
+
+    fps.toggle = {
+      mod = "Shift";
+      key = "F10";
+      cmd = "mytogglefps";
     };
+
     screenshot = {
       screen = {
         key = "F11";
         cmd = "myscreenshot -s";
       };
+
       window = {
         mod = "Ctrl";
         key = "F11";
         cmd = "myscreenshot -w";
       };
+
       region = {
         mod = "Shift";
         key = "F11";
