d21a7bb88d3f15bc3c435b400e7386a9d1aba1f3
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-03-18 10:11:46 +0530

    feat(vm/waydroid)!: rm steam integration

--- modules/system/gui/vm/waydroid.nix
+++ modules/system/gui/vm/waydroid.nix
@@ -8,18 +8,6 @@
 let
   cfg = config.my.programs.vm;
 
-  res = {
-    desktop = {
-      width = my.gui.display.string.desktop.width;
-      height = my.gui.display.string.desktop.height;
-    };
-
-    steam = {
-      width = builtins.toString config.my.programs.steam.display.width;
-      height = builtins.toString config.my.programs.steam.display.height;
-    };
-  };
-
   sudo = "/run/wrappers/bin/sudo";
 
   waydroidPkg = lib.getExe pkgs.waydroid;
@@ -28,33 +16,14 @@ let
 
   mkWaydroidSetRes =
     {
-      w ? "",
-      h ? "",
+      w ? my.gui.display.string.desktop.width,
+      h ? my.gui.display.string.desktop.height,
     }:
     ''
       ${waydroidPkg} prop set persist.waydroid.width ${w}
       ${waydroidPkg} prop set persist.waydroid.height ${h}
       ${sudo} ${waydroidRestart}
     '';
-
-  mkWaydroidCageLaunch =
-    name: args:
-    let
-      waydroidLaunch = lib.getExe (
-        pkgs.writeShellScriptBin "waydroid-cage-lauch"
-          # sh
-          ''
-            ${lib.getExe pkgs.wlr-randr} --output X11-1 --custom-mode ${res.steam.width}x${res.steam.height}
-            sleep 2
-
-            ${waydroidPkg} ${builtins.toString args}
-          ''
-      );
-    in
-    pkgs.writeShellScriptBin name # sh
-      ''
-        ${lib.getExe pkgs.cage} -- ${waydroidLaunch}
-      '';
 in
 {
   options.my.programs.vm.waydroid.enable = lib.mkEnableOption "Waydroid";
@@ -65,62 +34,48 @@ in
       waydroidRestart
     ];
 
-    environment.systemPackages =
-      with pkgs;
-      (
-        [
-          wl-clipboard
-          python3Packages.pyclip
-          (writeShellScriptBin "waydroid-init" # sh
-            ''
-              set -e # exit on any error
+    environment.systemPackages = with pkgs; [
+      wl-clipboard
+      python3Packages.pyclip
 
-              ${sudo} ${waydroidPkg} init -s GAPPS
+      (writeShellScriptBin "waydroid-init" # sh
+        ''
+          set -e # exit on any error
 
-              # controller support (Allows access to all plugged in devices)
-              ${waydroidPkg} prop set persist.waydroid.udev true
-              ${waydroidPkg} prop set persist.waydroid.uevent true
+          ${sudo} ${waydroidPkg} init -s GAPPS
 
-              # Disable freeform apps (Only fullscreen apps)
-              ${waydroidPkg} prop set persist.waydroid.multi_windows false
+          # controller support (Allows access to all plugged in devices)
+          ${waydroidPkg} prop set persist.waydroid.udev true
+          ${waydroidPkg} prop set persist.waydroid.uevent true
 
-              ${mkWaydroidSetRes {
-                w = res.desktop.width;
-                h = res.desktop.height;
-              }}
+          # Disable freeform apps (Only fullscreen apps)
+          ${waydroidPkg} prop set persist.waydroid.multi_windows false
 
-              ${sudo} ${waydroidRestart}
+          ${mkWaydroidSetRes { }}
 
-              ${waydroidPkg} show-full-ui &
-              sleep 5
+          ${sudo} ${waydroidRestart}
 
-              # refer: https://docs.waydro.id/faq/google-play-certification
-              certify_url="https://www.google.com/android/uncertified"
+          ${waydroidPkg} show-full-ui &
+          sleep 5
 
-              android_id=$(${sudo} ${waydroidPkg} shell sqlite3 /data/data/com.google.android.gsf/databases/gservices.db "select * from main where name = \"android_id\";" | ${lib.getExe' pkgs.coreutils-full "cut"} -d '|' -f2)
-              "${lib.getExe' pkgs.wl-clipboard "wl-copy"}" "$android_id"
+          # refer: https://docs.waydro.id/faq/google-play-certification
+          certify_url="https://www.google.com/android/uncertified"
 
-              echo "Android ID (copied to clipboard): $android_id"
-              echo "Enter ID in this page: $certify_url"
+          android_id=$(${sudo} ${waydroidPkg} shell sqlite3 /data/data/com.google.android.gsf/databases/gservices.db "select * from main where name = \"android_id\";" | ${lib.getExe' pkgs.coreutils-full "cut"} -d '|' -f2)
+          "${lib.getExe' pkgs.wl-clipboard "wl-copy"}" "$android_id"
 
-              "${lib.getExe' pkgs.xdg-utils "xdg-open"}" "$certify_url"
-            ''
-          )
+          echo "Android ID (copied to clipboard): $android_id"
+          echo "Enter ID in this page: $certify_url"
 
-          (writeShellScriptBin "waydroid-reset-res" (mkWaydroidSetRes { }))
-          (writeShellScriptBin "waydroid-set-res" (mkWaydroidSetRes {
-            w = res.desktop.width;
-            h = res.desktop.height;
-          }))
-        ]
-        ++ (lib.optional config.programs.steam.gamescopeSession.enable [
-          (mkWaydroidCageLaunch "waydroid-cage" [ "show-full-ui" ])
-          (makeDesktopItem rec {
-            name = "Waydroid Cage";
-            desktopName = name;
-            exec = "waydroid-cage";
-          })
-        ])
-      );
+          "${lib.getExe' pkgs.xdg-utils "xdg-open"}" "$certify_url"
+        ''
+      )
+
+      (writeShellScriptBin "waydroid-reset-res" (mkWaydroidSetRes {
+        w = "";
+        h = "";
+      }))
+      (writeShellScriptBin "waydroid-set-res" (mkWaydroidSetRes { }))
+    ];
   };
 }
