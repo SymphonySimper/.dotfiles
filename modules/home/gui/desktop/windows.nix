5ad1f0c34aace630642f779d08ff9d7d7b249cca
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-03-18 16:58:30 +0530

    refactor(desktop/windows): better format

--- modules/home/gui/desktop/windows.nix
+++ modules/home/gui/desktop/windows.nix
@@ -2,23 +2,20 @@ let
   mkChromiumAppId = url: "chrome-${url}__-Default";
 in
 {
-  "1" = {
-    class = [
-      "Alacritty"
-    ];
-  };
-  "2" = {
-    class = [
-      "google-chrome"
-      "chromium-browser"
-    ];
-  };
-  "3" = {
-    class = [
-      "com.github.johnfactotum.Foliate"
-      "org.pwmt.zathura"
-    ];
-  };
+  "1".class = [
+    "Alacritty"
+  ];
+
+  "2".class = [
+    "google-chrome"
+    "chromium-browser"
+  ];
+
+  "3".class = [
+    "com.github.johnfactotum.Foliate"
+    "org.pwmt.zathura"
+  ];
+
   "4" = {
     class = [
       "mpv"
@@ -29,39 +26,35 @@ in
       ".*Opera"
     ];
   };
-  "5" = {
-    class = [
-      "steam"
-      "thunderbird"
-    ];
-  };
+
+  "5".class = [
+    "steam"
+    "thunderbird"
+  ];
+
   "6" = {
     class = [ "Waydroid" ];
     title = [ "Minecraft" ];
   };
-  "7" = {
-    class = [
-      "discord"
 
-      # steam
-      "steam_app_.*"
-      "gamescope"
-    ];
-  };
-  "8" = {
-    class = [
-      (mkChromiumAppId "music.youtube.com")
-    ];
-  };
-  "9" = {
-    class = [
-      "org.qbittorrent.qBittorrent"
-      "com.obsproject.Studio"
-    ];
-  };
-  "0" = {
-    title = [
-      ".*sharing your screen.*"
-    ];
-  };
+  "7".class = [
+    "discord"
+
+    # steam
+    "steam_app_.*"
+    "gamescope"
+  ];
+
+  "8".class = [
+    (mkChromiumAppId "music.youtube.com")
+  ];
+
+  "9".class = [
+    "org.qbittorrent.qBittorrent"
+    "com.obsproject.Studio"
+  ];
+
+  "0".title = [
+    ".*sharing your screen.*"
+  ];
 }
