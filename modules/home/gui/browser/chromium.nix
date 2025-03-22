e4152b18ee9fd534137b7123f812b6031c28cc6d
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-03-22 20:57:49 +0530

    feat(browser/chromium): add systemd service to kill chromium browsers on poweroff/reboot

--- modules/home/gui/browser/chromium.nix
+++ modules/home/gui/browser/chromium.nix
@@ -1,12 +1,54 @@
-{ pkgs, ... }:
 {
-  programs.chromium = {
-    enable = true;
-    package = pkgs.google-chrome;
+  my,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  name = "my-kill-chromium-gracefully";
+  script =
+    pkgs.writeShellScriptBin name # sh
+      ''
+        browsers=('chrome' 'chromium' 'brave')
+        for browser in "''${browsers[@]}"; do
+          if [[ -n "$(${lib.getExe' pkgs.toybox "pgrep"} $browser)" ]]; then
+            ${lib.getExe pkgs.killall} "$browser" --wait
+          fi
+        done
+      '';
+in
+lib.mkMerge [
+  {
+    programs.chromium = {
+      enable = true;
+      package = pkgs.google-chrome;
 
-    commandLineArgs = [
-      "--ozone-platform-hint=auto"
-      "--disable-features=WebRtcAllowInputVolumeAdjustment"
-    ];
-  };
-}
+      commandLineArgs = [
+        "--ozone-platform-hint=auto"
+        "--disable-features=WebRtcAllowInputVolumeAdjustment"
+      ];
+    };
+  }
+
+  (lib.mkIf my.gui.desktop.enable {
+    # refer: https://wiki.gentoo.org/wiki/Chrome#Troubleshooting
+    systemd.user.services."${name}" = {
+      Unit = {
+        Description = "Help Chrome close gracefully";
+        DefaultDependencies = "no";
+        Before = "shutdown.target";
+      };
+
+      Service = {
+        Type = "oneshot";
+        ExecStart = lib.getExe script;
+      };
+
+      Install.WantedBy = [
+        "halt.target"
+        "reboot.target"
+        "shutdown.target"
+      ];
+    };
+  })
+]
