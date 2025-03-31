00b84d8e3367c9ae80602422a7078dfc623431ad
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-04-01 04:45:51 +0530

    feat(hardware/powerManagement): rm tlp and enable power-profiles-daemon

--- modules/system/hardware/powerManagement.nix
+++ modules/system/hardware/powerManagement.nix
@@ -13,40 +13,7 @@ in
     services = {
       # TODO: enable whe cpu.intel is enabled
       thermald.enable = false;
-
-      tlp = {
-        enable = true;
-        # refer: https://linrunner.de/tlp/support/optimizing.html#extend-battery-runtime
-        settings = {
-          # CPU
-          CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
-          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-          # Platform
-          PLATFORM_PROFILE_ON_AC = "balanced";
-          PLATFORM_PROFILE_ON_BAT = "low-power";
-
-          # Disable turbo boost
-          CPU_BOOST_ON_AC = 1;
-          CPU_BOOST_ON_BAT = 0;
-          CPU_HWP_DYN_BOOST_ON_AC = 1;
-          CPU_HWP_DYN_BOOST_ON_BAT = 0;
-
-          # ABM
-          AMDGPU_ABM_LEVEL_ON_AC = 0;
-          AMDGPU_ABM_LEVEL_ON_BAT = 3;
-
-          # Runtime
-          RUNTIME_PM_ON_AC = "auto";
-          RUNTIME_PM_ON_BAT = "auto";
-
-          # # WiFi
-          # WIFI_PWR_ON_AC = "on";
-          # WIFI_PWR_ON_BAT = "on";
-        };
-      };
-
-      power-profiles-daemon.enable = false;
+      power-profiles-daemon.enable = true;
     };
   };
 }
