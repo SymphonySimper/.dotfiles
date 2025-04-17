ac13b98626fb2f4abc6f2bbfd80e67b29604600e
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-04-17 13:24:07 +0530

    feat(cli/docker): add option to enable rootless and add docker group

--- modules/system/cli/docker.nix
+++ modules/system/cli/docker.nix
@@ -4,24 +4,43 @@
   lib,
   ...
 }:
+let
+  cfg = config.my.programs.docker;
+in
 {
   options.my.programs.docker = {
     enable = lib.mkEnableOption "Docker";
+    enableRootless = lib.mkEnableOption "Rootless";
+    enableGroup = lib.mkEnableOption "Docker group";
   };
 
-  config = lib.mkIf config.my.programs.docker.enable {
-    virtualisation.docker = {
-      enable = true;
-      enableOnBoot = lib.mkDefault false;
-      liveRestore = false;
+  config = lib.mkIf cfg.enable {
 
-      autoPrune.enable = true;
-
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
+    my = {
+      programs.docker = {
+        enableRootless = lib.mkDefault true;
+        enableGroup = lib.mkDefault false;
       };
+
+      user.groups = lib.optionals cfg.enableGroup [ "docker" ];
     };
+
+    virtualisation.docker = lib.mkMerge [
+      {
+        enable = true;
+        enableOnBoot = lib.mkDefault false;
+        liveRestore = false;
+
+        autoPrune.enable = true;
+      }
+
+      (lib.mkIf cfg.enableRootless {
+        rootless = {
+          enable = true;
+          setSocketVariable = true;
+        };
+      })
+    ];
 
     environment.systemPackages = [
       (pkgs.writeShellScriptBin "mydocker" # sh
