f760166c9777bf99134099bf7419759c30c6087f
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-04-18 17:33:54 +0530

    feat(hardware/bluetooth): add cli to manage devices

--- modules/system/hardware/bluetooth.nix
+++ modules/system/hardware/bluetooth.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   options.my.hardware.bluetooth.enable = lib.mkEnableOption "Bluetooth";
   config = lib.mkIf config.my.hardware.bluetooth.enable {
@@ -6,5 +11,11 @@
       enable = true; # enables support for Bluetooth
       powerOnBoot = true; # powers up the default Bluetooth controller on boot
     };
+
+    # services.blueman.enable = true; # gui
+    environment.systemPackages = [
+      # pkgs.overskride # gui
+      pkgs.bluetui
+    ];
   };
 }
