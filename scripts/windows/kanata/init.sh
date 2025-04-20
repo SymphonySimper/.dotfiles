70527a3f1b9252ec9d6a083c8b4d90614b82eccc
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-04-20 18:10:06 +0530

    feat: mv .windows to scripts/windows

--- scripts/windows/kanata/init.sh
+++ scripts/windows/kanata/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp -vr ~/.dotfiles/.windows/kanata /mnt/c/Users/symph/Documents/
+cp -vr ~/.dotfiles/scripts/windows/kanata /mnt/c/Users/symph/Documents/
 cp -v ./kanata-run-shortcut.lnk "/mnt/c/Users/symph/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/"
 cp -v ./kanata-run-shortcut.lnk "/mnt/c/Users/symph/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/"
