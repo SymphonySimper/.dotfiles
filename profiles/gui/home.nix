1d75b7a63f332754d9b49fd2f5b861c16d2a77ca
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-04-24 21:10:26 +0530

    feat(profiles/gui): add google chrome

--- profiles/gui/home.nix
+++ profiles/gui/home.nix
@@ -1,6 +1,20 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
   imports = [
     ../../modules/home
+  ];
+
+  # Add Google Chrome
+  my.desktop.windows = [
+    {
+      id = "google-chrome";
+      workspace = 3;
+    }
+  ];
+
+  home.packages = [
+    (pkgs.google-chrome.override {
+      commandLineArgs = builtins.concatStringsSep " " config.my.programs.browser.args;
+    })
   ];
 }
