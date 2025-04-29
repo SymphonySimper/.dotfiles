6e8a88aa24429ff52d693f0f46c2efcb283b68ed
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-04-30 01:15:34 +0530

    feat(cli/shell)!: rm im_light alias

--- modules/home/cli/shell/default.nix
+++ modules/home/cli/shell/default.nix
@@ -74,7 +74,5 @@
     ## ls
     ls = "ls --almost-all --color=yes --group-directories-first --human-readable";
     lsl = "${ls} -l --size";
-    ## misc
-    im_light = "${lib.getExe pkgs.ps_mem} -p $(pgrep -d, -u $USER)";
   };
 }
