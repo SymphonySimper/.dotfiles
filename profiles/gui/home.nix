51c2dee32bacba28a3fdd8a9d69d3eaa7832487e
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-05-02 12:50:45 +0530

    feat(profiles/gui): auto start chrome on weekdays

--- profiles/gui/home.nix
+++ profiles/gui/home.nix
@@ -5,16 +5,27 @@
   ];
 
   # Add Google Chrome
-  my.desktop.windows = [
-    {
-      id = "google-chrome";
-      workspace = 3;
-    }
-  ];
-
   home.packages = [
     (pkgs.google-chrome.override {
       commandLineArgs = builtins.concatStringsSep " " config.my.programs.browser.chromiumArgs;
     })
   ];
+
+  my.desktop = {
+    autostart = [
+      {
+        name = "google-chrome";
+        cmd = "google-chrome-stable";
+        days = [1 2 3 4 5];
+      }
+    ];
+
+    windows = [
+      {
+        id = "google-chrome";
+        workspace = 3;
+        silent = true;
+      }
+    ];
+  };
 }
