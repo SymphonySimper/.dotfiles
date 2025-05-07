040ce11678ad628b9682a83035c53b2ba41d9f07
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-05-07 22:09:01 +0530

    feat(home/gui)!: mv mpv, obs to video

--- modules/home/gui/default.nix
+++ modules/home/gui/default.nix
@@ -3,33 +3,18 @@
   imports = [
     ./alacritty.nix
     ./browser.nix
-    ./mpv.nix
     ./music.nix
     ./office.nix
     ./theme.nix
+    ./video.nix
 
     ./desktop
   ];
 
-  config = lib.mkIf my.gui.enable (
-    lib.mkMerge [
-      {
-        my.programs = {
-          browser.enable = lib.mkDefault my.gui.enable;
-          music.enable = lib.mkDefault my.gui.enable;
-          office.enable = lib.mkDefault my.gui.enable;
-        };
-      }
-
-      {
-        programs.obs-studio.enable = true;
-        my.desktop.windows = [
-          {
-            id = "com.obsproject.Studio";
-            workspace = 9;
-          }
-        ];
-      }
-    ]
-  );
+  my.programs = {
+    browser.enable = lib.mkDefault my.gui.enable;
+    music.enable = lib.mkDefault my.gui.enable;
+    office.enable = lib.mkDefault my.gui.enable;
+    video.enable = lib.mkDefault my.gui.enable;
+  };
 }
