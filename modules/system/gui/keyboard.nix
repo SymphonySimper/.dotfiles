935877a33eaf6dd7ccbf90ba4acb0db07c032449
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-05-14 21:06:04 +0530

    fix(gui/keyboard): disable trackpad when typing

--- modules/system/gui/keyboard.nix
+++ modules/system/gui/keyboard.nix
@@ -1,4 +1,4 @@
-{ lib, my, ... }:
+{ my, lib, ... }:
 {
   config = lib.mkIf my.gui.desktop.enable {
     services.keyd = {
@@ -9,5 +9,14 @@
         settings.main.capslock = "overload(control, esc)";
       };
     };
+
+    # refer: https://github.com/rvaiya/keyd?tab=readme-ov-file#why-is-my-trackpad-is-interfering-with-input-after-enabling-keyd
+    environment.etc."libinput/local-overrides.quirks".text = ''
+      [Serial Keyboards]
+
+      MatchUdevType=keyboard
+      MatchName=keyd*keyboard
+      AttrKeyboardIntegration=internal
+    '';
   };
 }
