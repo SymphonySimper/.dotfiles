12d1a263b681fd0d3731fd1a57b1d6935087ba51
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-05-16 23:47:22 +0530

    feat(cli/scripts): add myocr

---
+++ modules/home/cli/scripts/ocr.nix
@@ -0,0 +1,17 @@
+{ pkgs, lib, ... }:
+let
+  app = lib.getExe pkgs.tesseract;
+  wlpaste = lib.getExe' pkgs.wl-clipboard "wl-paste";
+in
+# sh
+''
+  lang="eng"
+  if [[ -n "$2" ]]; then
+    lang="$2"
+  fi
+
+  case "$1" in
+    pipe) ${wlpaste} | ${app} stdin - -l "$lang" ;;
+    *) ${app} "$1" - -l "$lang" ;;
+  esac
+''
