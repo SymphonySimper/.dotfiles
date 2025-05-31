d6213622f3841758d700ec7800742f6e904c3e89
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-05-31 15:02:07 +0530

    feat(system/boot)!: add option to set kenerl and set default kernel to LTS

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -7,21 +7,33 @@
 }:
 let
   cfg = config.my.boot;
+
+  # for more versions: https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/linux-kernels.nix
+  kernels = {
+    lts = pkgs.linuxPackages;
+    latest = pkgs.linuxPackages_latest;
+  };
 in
 {
   options.my.boot = {
     enable = lib.mkEnableOption "boot";
+
+    kernel = lib.mkOption {
+      description = "Kernel to use";
+      type = lib.types.oneOf [
+        (lib.types.enum (builtins.attrNames kernels))
+        lib.types.raw
+      ];
+      default = "lts";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     catppuccin.tty.enable = my.theme.dark;
 
-    # Bootloader.
     boot = {
-      # lts: `pkgs.linuxPackages`
-      # latest: `pkgs.linuxPackages_latest`
-      # for rest visit: https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/linux-kernels.nix
-      kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+      kernelPackages =
+        if builtins.typeOf cfg.kernel == "string" then kernels.${cfg.kernel} else cfg.kernel;
       loader = {
         systemd-boot = {
           enable = true;
