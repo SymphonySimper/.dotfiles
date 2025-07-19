f7be1970317f1edff7610128305bb6f9d55b93ad
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-07-19 13:36:45 +0530

    refactor(home/editor): sort space keybinds

--- modules/home/editor.nix
+++ modules/home/editor.nix
@@ -380,36 +380,36 @@ in
             space = {
               b = {
                 c = ":bc";
-                w = ":w";
                 r = ":reload";
                 R = ":reload-all";
+                w = ":w";
               };
 
               c = {
-                c = "toggle_comments";
-                C = "toggle_block_comments";
-                f = ":format";
                 a = "code_action";
-                r = "rename_symbol";
-                l = ":lsp-restart";
-                h = "select_references_to_symbol_under_cursor";
                 b = ":sh ${lib.getExe pkgs.git} -C $(dirname $(realpath %{buffer_name})) blame -L %{cursor_line},%{cursor_line} $(realpath %{buffer_name})";
+                C = "toggle_block_comments";
+                c = "toggle_comments";
+                f = ":format";
+                h = "select_references_to_symbol_under_cursor";
+                l = ":lsp-restart";
+                r = "rename_symbol";
                 s = "signature_help";
                 y = ":yank-diagnostic";
               };
 
               f = {
                 "'" = "last_picker";
-                f = "file_picker";
-                F = "file_picker_in_current_buffer_directory"; # prev: file_picker_in_current_directory
                 b = "buffer_picker";
-                j = "jumplist_picker";
-                g = "changed_file_picker";
-                s = "symbol_picker";
-                S = "workspace_symbol_picker";
                 d = "diagnostics_picker";
                 D = "workspace_diagnostics_picker";
+                f = "file_picker";
+                F = "file_picker_in_current_buffer_directory"; # prev: file_picker_in_current_directory
+                g = "changed_file_picker";
                 "/" = "global_search";
+                j = "jumplist_picker";
+                s = "symbol_picker";
+                S = "workspace_symbol_picker";
                 y = ":sh ${lib.getExe pkgs.tmux} new-window ${lib.getExe' pkgs.yazi "yazi"} $(realpath %{buffer_name}) ";
               };
 
