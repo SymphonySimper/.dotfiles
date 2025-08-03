6e37c6ad84a201e7d237d80eaa095a437e17c278
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-08-03 16:51:28 +0530

    feat(system/desktop)!: force disable displayManager

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -2,42 +2,54 @@
 {
   options.my.programs.desktop.enable = lib.mkEnableOption "Desktop";
 
-  config = lib.mkIf config.my.programs.desktop.enable {
-    programs.hyprland = {
-      enable = true;
-      withUWSM = true;
-    };
-
-    security = {
-      polkit.enable = true;
-      pam.services = {
-        hyprland.enableGnomeKeyring = true;
-        # Enable hyprlock
-        hyprlock = { };
-      };
-    };
-
-    services.udisks2.enable = true;
-
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-    my = {
-      boot.enable = true;
-
-      hardware = {
-        audio = {
+  config = lib.mkIf config.my.programs.desktop.enable (
+    lib.mkMerge [
+      {
+        programs.hyprland = {
           enable = true;
-          programs.enable = lib.mkDefault true;
+          withUWSM = true;
         };
 
-        keyboard.enable = true;
-        powerManagement.enable = true;
-      };
+        security = {
+          polkit.enable = true;
+          pam.services = {
+            hyprland.enableGnomeKeyring = true;
+            # Enable hyprlock
+            hyprlock = { };
+          };
+        };
 
-      programs = {
-        browser.enable = true;
+        services = {
+          udisks2.enable = true;
+        };
 
-        tty."1" = {
+        environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+        my = {
+          boot.enable = true;
+
+          hardware = {
+            audio = {
+              enable = true;
+              programs.enable = lib.mkDefault true;
+            };
+
+            keyboard.enable = true;
+            powerManagement.enable = true;
+          };
+
+          programs = {
+            browser.enable = true;
+          };
+        };
+      }
+
+      {
+        # uwsm module enables display manager.
+        # which then overrides tty1 (https://github.com/NixOS/nixpkgs/pull/429845)
+        services.displayManager.enable = lib.mkForce false;
+
+        my.programs.tty."1" = {
           skipUsername = true;
           launch =
             let
@@ -53,7 +65,7 @@
               dbus = false;
             };
         };
-      };
-    };
-  };
+      }
+    ]
+  );
 }
