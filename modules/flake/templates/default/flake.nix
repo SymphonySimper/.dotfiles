478a76cc0d17df127d4dc1003bd6d7487a89be0d
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-08-16 01:46:35 +0530

    feat(templates/default)!: rm just

--- modules/flake/templates/default/flake.nix
+++ modules/flake/templates/default/flake.nix
@@ -38,8 +38,6 @@
 
             # TODO: Update packages and shellHook
             packages = with pkgs; [
-              just # do not remove
-
               # replace with packages that you need in env
               cowsay
             ];
