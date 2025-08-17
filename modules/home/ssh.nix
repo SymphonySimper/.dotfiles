2b7cd87101117657a87a9a414e0cdd1b2950ecba
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-08-17 22:16:41 +0530

    feat(home/ssh)!: add alias to programs.ssh and add option to config dir of ssh

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -1,28 +1,49 @@
-{ ... }:
 {
-  services.ssh-agent.enable = true;
+  my,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.my.programs.ssh;
+in
+{
+  imports = [
+    (lib.modules.mkAliasOptionModule [ "my" "programs" "ssh" ] [ "programs" "ssh" ])
+  ];
 
-  programs.ssh = {
-    enable = true;
-    addKeysToAgent = "yes";
-
-    matchBlocks = {
-      "github" = {
-        hostname = "github.com";
-        identityFile = "~/.ssh/id_ed25519";
-        identitiesOnly = true;
-      };
-
-      "work-github" = {
-        hostname = "github.com";
-        identityFile = "~/.ssh/work_id_ed25519";
-        identitiesOnly = true;
-      };
-    };
+  options.my.programs.ssh.dir = lib.mkOption {
+    type = lib.types.str;
+    description = "Directory where config id files are stored.";
+    readOnly = true;
+    default = "${my.dir.home}/.ssh";
   };
 
-  my.programs.scripts.reload.commands."SSH Agent" = {
-    cmd = "systemctl restart --user ssh-agent.service";
-    check = "ssh-agent";
+  config = {
+    services.ssh-agent.enable = true;
+
+    programs.ssh = {
+      enable = true;
+      addKeysToAgent = "yes";
+
+      matchBlocks = {
+        "github" = {
+          hostname = "github.com";
+          identityFile = "${cfg.dir}/id_ed25519";
+          identitiesOnly = true;
+        };
+
+        "work-github" = {
+          hostname = "github.com";
+          identityFile = "${cfg.dir}/work_id_ed25519";
+          identitiesOnly = true;
+        };
+      };
+    };
+
+    my.programs.scripts.reload.commands."SSH Agent" = {
+      cmd = "systemctl restart --user ssh-agent.service";
+      check = "ssh-agent";
+    };
   };
 }
