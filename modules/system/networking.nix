9fd3c2b89295ca17b249435c7f4a5fc79995d61e
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-09-15 11:52:13 +0530

    feat(system/networking): make module modular

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -8,22 +8,51 @@ let
   cfg = config.my.networking;
 in
 {
-  options.my.networking.enable = lib.mkEnableOption "networking";
+  options.my.networking = {
+    enable = lib.mkEnableOption "networking";
+    useDHCP = lib.mkEnableOption "useDHCP";
+    nm.enable = lib.mkEnableOption "NetworkManager";
+    firewall.enable = lib.mkEnableOption "firewall";
 
-  config = lib.mkIf cfg.enable {
-    my.user.sudo.nopasswd = [ "${config.my.user.bin}/systemctl restart NetworkManager" ];
-
-    networking = {
-      hostName = lib.mkDefault my.profile;
-      useDHCP = lib.mkDefault true;
-      networkmanager.enable = true;
-
-      firewall = {
-        enable = true;
-        allowedTCPPorts = [
-          5173 # vite
-        ];
-      };
+    hostName = lib.mkOption {
+      type = lib.types.str;
+      description = "Hostname";
+      default = my.profile;
     };
   };
+
+  config = lib.mkMerge [
+    {
+      networking = {
+        hostName = cfg.hostName;
+      };
+    }
+
+    (lib.mkIf cfg.enable {
+      my = {
+        user.sudo.nopasswd = lib.lists.optional (cfg.nm.enable) "${config.my.user.bin}/systemctl restart NetworkManager";
+
+        networking = {
+          useDHCP = lib.mkDefault true;
+          nm.enable = lib.mkDefault true;
+          firewall.enable = lib.mkDefault true;
+        };
+      };
+
+      networking = {
+        useDHCP = lib.mkDefault cfg.useDHCP;
+
+        networkmanager = {
+          enable = cfg.nm.enable;
+        };
+
+        firewall = {
+          enable = cfg.firewall.enable;
+          allowedTCPPorts = [
+            5173 # vite
+          ];
+        };
+      };
+    })
+  ];
 }
