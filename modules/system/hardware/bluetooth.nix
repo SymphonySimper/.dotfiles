50137d57e5a319c604ed027ed54dfe00a1837e20
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-09-20 01:40:22 +0530

    feat(hardware/bluetooth)!: add option to enable blueman
    No longer enables blueman by default

--- modules/system/hardware/bluetooth.nix
+++ modules/system/hardware/bluetooth.nix
@@ -4,18 +4,31 @@
   lib,
   ...
 }:
+let
+  cfg = config.my.hardware.bluetooth;
+in
 {
-  options.my.hardware.bluetooth.enable = lib.mkEnableOption "Bluetooth";
-  config = lib.mkIf config.my.hardware.bluetooth.enable {
-    hardware.bluetooth = {
-      enable = true; # enables support for Bluetooth
-      powerOnBoot = true; # powers up the default Bluetooth controller on boot
-    };
-
-    services.blueman.enable = true; # gui
-    environment.systemPackages = [
-      # pkgs.overskride # gui
-      pkgs.bluetui
-    ];
+  options.my.hardware.bluetooth = {
+    enable = lib.mkEnableOption "Bluetooth";
+    blueman.enable = lib.mkEnableOption "Blueman";
   };
+
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        hardware.bluetooth = {
+          enable = true; # enables support for Bluetooth
+          powerOnBoot = true; # powers up the default Bluetooth controller on boot
+        };
+      }
+
+      (lib.mkIf cfg.blueman.enable {
+        services.blueman.enable = true; # gui
+        environment.systemPackages = [
+          # pkgs.overskride # gui
+          pkgs.bluetui
+        ];
+      })
+    ]
+  );
 }
