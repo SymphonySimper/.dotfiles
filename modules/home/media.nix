a5440a263a7ac2826098f11c90e7e2c0fc51fcb8
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-09-20 02:15:13 +0530

    feat(home/media)!: rm media.video (mpv, yt-dlp)
    Gnome comes with a video player. Which is good enough for my current
    use.

--- modules/home/media.nix
+++ modules/home/media.nix
@@ -1,10 +1,4 @@
-{
-  my,
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, lib, ... }:
 let
   cfg = config.my.programs.media;
 in
@@ -12,7 +6,6 @@ in
   options.my.programs.media = {
     enable = lib.mkEnableOption "Media";
     recorder.enable = lib.mkEnableOption "Recorder";
-    video.enable = lib.mkEnableOption "Video";
   };
 
   config = lib.mkIf cfg.enable (
@@ -21,7 +14,6 @@ in
         my.programs = {
           media = {
             recorder.enable = lib.mkDefault false;
-            video.enable = lib.mkDefault true;
           };
         };
       }
@@ -35,81 +27,6 @@ in
             workspace = 5;
           }
         ];
-      })
-
-      (lib.mkIf cfg.video.enable {
-        my.programs.desktop.automove = [
-          {
-            name = "mpv.desktop";
-            workspace = 5;
-          }
-        ];
-
-        catppuccin.mpv.enable = false;
-        programs.mpv = {
-          enable = true;
-
-          package = (
-            pkgs.mpv-unwrapped.wrapper {
-              mpv = pkgs.mpv-unwrapped.override {
-                ffmpeg = pkgs.ffmpeg-full;
-                waylandSupport = true;
-              };
-            }
-          );
-
-          bindings = {
-            "Alt+LEFT" = "script-message Cycle_Video_Rotate -90";
-            "Alt+RIGHT" = "script-message Cycle_Video_Rotate 90";
-          };
-
-          config = {
-            fullscreen = "yes";
-
-            # OSD / OSC
-            osc = "no";
-            osd-font-size = 32;
-            osd-border-size = 1.5;
-            osd-font = my.theme.font.sans;
-
-            # Subtitles
-            sub-visibility = "yes";
-            sub-auto = "fuzzy"; # external subs don't have to match the file name exactly to autoload
-            sub-file-paths-append = [
-              "ass" # search for external subs in these relative subdirectories
-              "srt"
-              "sub"
-              "subs"
-              "subtitles"
-            ];
-
-            sub-font = my.theme.font.sans;
-            sub-font-size = 48;
-            sub-border-size = 2.0;
-
-            # Hardware acceleration
-            hwdec = "auto-safe";
-            vo = "gpu";
-            profile = "gpu-hq";
-            gpu-context = "wayland";
-
-            # Audio
-            audio-file-auto = "fuzzy"; # external audio doesn't has to match the file name exactly to autoload
-            audio-pitch-correction = "yes"; # automatically insert scaletempo when playing with higher speed
-
-            # Border
-            border = "no";
-
-            background-color = "#000000";
-          };
-        };
-      })
-
-      (lib.mkIf cfg.video.enable {
-        programs = {
-          yt-dlp.enable = true;
-          mpv.config.script-opts = "ytdl_hook-ytdl_path=${lib.getExe config.programs.yt-dlp.package}";
-        };
       })
     ]
   );
