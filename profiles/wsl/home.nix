b0fb5390b787aaf6bd87f0aa07c856d4a876693c
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-10-07 21:09:18 +0530

    feat(profiles/wsl): set editor's clipboard provider to win32yank's absolute path

--- profiles/wsl/home.nix
+++ profiles/wsl/home.nix
@@ -1,4 +1,24 @@
 { ... }:
 {
-  my.programs.editor.clipboardProvider = "win32-yank";
+  # refer: https://github.com/helix-editor/helix/blob/5b0563419eeeaf0595c848865c46be4abad246a7/helix-view/src/clipboard.rs#L401-L402
+  my.programs.editor.clipboardProvider =
+    let
+      command = "/mnt/c/Program Files/Neovim/bin/win32yank.exe";
+    in
+    {
+      yank = {
+        inherit command;
+        args = [
+          "-o"
+          "--lf"
+        ];
+      };
+      paste = {
+        inherit command;
+        args = [
+          "-i"
+          "--crlf"
+        ];
+      };
+    };
 }
