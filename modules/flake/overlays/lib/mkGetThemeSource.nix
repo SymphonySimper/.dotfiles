40a1de62128da6c6f506d5ac1b6c273e8fbee909
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-10-22 19:36:29 +0530

    feat(lib/mkGetThemeSource): add accent support and option to capitlaize all

--- modules/flake/overlays/lib/mkGetThemeSource.nix
+++ modules/flake/overlays/lib/mkGetThemeSource.nix
@@ -8,15 +8,26 @@ let
     {
       package,
       filename,
-      capitalizeFlavor ? false,
-      capitalizeName ? false,
+      capitalize ? false,
+      capitalizeFlavor ? null,
+      capitalizeName ? null,
+      capitalizeAccent ? null,
     }:
     let
-      rawName = "catppuccin";
-      name = if capitalizeName then lib.strings.toSentenceCase rawName else rawName;
-      flavor = if capitalizeFlavor then lib.strings.toSentenceCase my.theme.flavor else my.theme.flavor;
+      mkCapitalize =
+        conditionValue: value:
+        let
+          condition = if conditionValue == null then capitalize else conditionValue;
+        in
+        if condition then lib.strings.toSentenceCase value else value;
 
-      formattedFileName = builtins.replaceStrings [ "NAME" "FLAVOR" ] [ name flavor ] filename;
+      name = mkCapitalize capitalizeName "catppuccin";
+      flavor = mkCapitalize capitalizeFlavor my.theme.flavor;
+      accent = mkCapitalize capitalizeAccent my.theme.accent;
+
+      formattedFileName =
+        builtins.replaceStrings [ "NAME" "FLAVOR" "ACCENT" ] [ name flavor accent ]
+          filename;
     in
     (builtins.readFile "${inputs.catppuccin.packages.${my.system}.${package}}/${formattedFileName}");
 in
