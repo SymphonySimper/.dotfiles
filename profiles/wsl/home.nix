77ce391db4caa05b1fc0611662c18355e2ebd830
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-11-16 21:56:56 +0530

    feat(profiles/wsl): add script to link terminal from windows

--- profiles/wsl/home.nix
+++ profiles/wsl/home.nix
@@ -1,9 +1,16 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = [
     (pkgs.writeShellScriptBin "win32yank.exe" ''
       /mnt/c/Program\ Files/Neovim/bin/win32yank.exe  "''${@}"
     '')
+
+    (
+      let
+        command = config.my.programs.terminal.command;
+      in
+      pkgs.writeShellScriptBin "${command}" ''/mnt/c/Program\ Files/${command}/${command}.exe  "''${@}" ''
+    )
   ];
 
   my.programs.editor.clipboardProvider = "win32-yank";
