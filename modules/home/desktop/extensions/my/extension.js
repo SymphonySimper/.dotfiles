3733984c825790517c76df921f4649719e6e7eec
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-11-21 21:51:00 +0530

    feat(extensions/my): make all notifications as critical
    - Doesn't dismiss without user action
    - Show's notification in fullscreen

--- modules/home/desktop/extensions/my/extension.js
+++ modules/home/desktop/extensions/my/extension.js
@@ -1,4 +1,6 @@
+import { InjectionManager } from "resource:///org/gnome/shell/extensions/extension.js";
 import { panel, overview } from "resource:///org/gnome/shell/ui/main.js";
+import { Urgency, Source } from "resource:///org/gnome/shell/ui/messageTray.js";
 
 // panel visiblity logic is based on: https://github.com/fthx/panel-free
 class HidePanel {
@@ -32,10 +34,33 @@ class ShowOverviewOnEnable {
   disable() {}
 }
 
+class MakeAllNotificationsAsCritical {
+  #injectionManager = new InjectionManager();
+
+  enable() {
+    this.#injectionManager.overrideMethod(
+      Source.prototype,
+      "addNotification",
+      (addNotification) => {
+        return function (notification) {
+          notification.urgency = Urgency.CRITICAL;
+          addNotification.call(this, notification); // original method
+        };
+      },
+    );
+  }
+
+  disable() {
+    this.#injectionManager.clear();
+  }
+}
+
 export default class MyExtension {
-  #extensions = [HidePanel, ShowOverviewOnEnable].map(
-    (extension) => new extension(),
-  );
+  #extensions = [
+    HidePanel,
+    ShowOverviewOnEnable,
+    MakeAllNotificationsAsCritical,
+  ].map((extension) => new extension());
 
   enable() {
     this.#extensions.forEach((extension) => extension.enable());
