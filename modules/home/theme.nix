97ec8e8792e39546cb95ecd7cca61ce15b18cc81
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-11-25 13:13:54 +0530

    feat(home/theme)!: install only regular, italic and bold* version of nerd-fonts

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -14,8 +14,23 @@ in
     lib.mkMerge [
       {
         home.packages = with pkgs; [
-          nerd-fonts.jetbrains-mono
           noto-fonts-cjk-sans
+
+          (nerd-fonts.jetbrains-mono.overrideAttrs {
+            # refer: https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerd-fonts/default.nix#L65
+            postInstall =
+              let
+                variants = builtins.concatStringsSep "\\|" [
+                  "Regular"
+                  "Italic"
+                  "Bold.*"
+                ];
+              in
+              # sh
+              ''
+                find "$dst_truetype" -type f -not -regex ".*JetBrainsMonoNerdFont-\(${variants}\).ttf" -delete
+              '';
+          })
         ];
       }
 
