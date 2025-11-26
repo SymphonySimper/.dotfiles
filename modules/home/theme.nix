e861aa6355196c9d00ebe92189aa1a5be47c8b20
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-11-27 00:12:18 +0530

    feat(home/theme)!: rm cursor, gtk, qt settings

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -4,11 +4,6 @@
   pkgs,
   ...
 }:
-let
-  gtkExtraSettings = {
-    gtk-application-prefer-dark-theme = my.theme.dark;
-  };
-in
 {
   config = lib.mkIf my.gui.enable (
     lib.mkMerge [
@@ -35,55 +30,11 @@ in
       }
 
       {
-        catppuccin = {
-          gtk.icon.enable = false;
-          kvantum.enable = false; # qt
-        };
-
-        home.pointerCursor = {
-          gtk.enable = true;
-          # x11.enable = true;
-          package = pkgs.adwaita-icon-theme;
-          name = "Adwaita";
-          size = 16;
-        };
-
-        gtk = {
-          enable = true;
-
-          theme = {
-            name = my.theme.gtk;
-            package = pkgs.gnome-themes-extra;
-          };
-
-          iconTheme = {
-            package = pkgs.adwaita-icon-theme;
-            name = "Adwaita";
-          };
-
-          gtk3.extraConfig = gtkExtraSettings;
-          gtk4.extraConfig = gtkExtraSettings; # required for non libadwaita applications
-
-          # font = {
-          #   name = "Sans";
-          #   size = 11;
-          # };
-        };
-
-        qt = {
-          enable = true;
-          platformTheme.name = "adwaita";
-          style.name = lib.toLower my.theme.gtk;
-        };
-
         dconf = {
           enable = true;
 
           settings."org/gnome/desktop/interface" = {
             color-scheme = if my.theme.dark then "prefer-dark" else "default";
-            cursor-theme = "Adwaita";
-            gtk-theme = my.theme.gtk;
-            icon-theme = "Adwaita";
             toolkit-accessibility = false;
           };
         };
