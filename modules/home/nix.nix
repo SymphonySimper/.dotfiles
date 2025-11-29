2a8d3706c596c1230e2d85b734c05342b9cbfa48
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-11-29 19:23:58 +0530

    feat(home/nix): disable all shell integrations for nix-index

--- modules/home/nix.nix
+++ modules/home/nix.nix
@@ -56,6 +56,9 @@ in
     nix-index = {
       enable = true;
       enableBashIntegration = false;
+      enableFishIntegration = false;
+      enableNushellIntegration = false;
+      enableZshIntegration = false;
     };
   };
 
