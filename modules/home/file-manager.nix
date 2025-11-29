aec17d6f3fa754eb52f358080b57c519ad9306bd
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-11-29 14:46:37 +0530

    feat(home/file-manager): rm jump-to-char plugin

--- modules/home/file-manager.nix
+++ modules/home/file-manager.nix
@@ -191,16 +191,6 @@
           }
         ];
       }
-      {
-        plugins.jump-to-char = pkgs.yaziPlugins.jump-to-char;
-        keymap.mgr.prepend_keymap = [
-          {
-            on = "f";
-            run = "plugin jump-to-char";
-            desc = "Jump to char";
-          }
-        ];
-      }
     ];
   };
 }
