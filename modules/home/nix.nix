0cb1cdb017bce17b3558d6ca919d4bea2251e696
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-02 11:23:33 +0530

    feat(home/nix)!: rm mynix
    - cpu: nix flake update <input-name>
    - new|init: nix flake <new|init> --template my#templates.<template-name>
    - update: nix flake update --commit-lock-file

--- modules/home/nix.nix
+++ modules/home/nix.nix
@@ -1,57 +1,5 @@
+{ my, pkgs, ... }:
 {
-  my,
-  pkgs,
-  lib,
-  ...
-}:
-let
-  mynix =
-    pkgs.writeShellScriptBin "mynix"
-      # sh
-      ''
-        function abort() {
-          printf "\n%s\n" "''${1:-Aborted.}";
-          exit 1;
-        }
-
-        case "$1" in
-          cpu)
-            readarray -t flake_inputs < <(nix flake metadata --json | ${lib.getExe pkgs.jq} -r ".locks.nodes.root.inputs | keys[]")
-            select input in "Abort" "''${flake_inputs[@]}"; do
-              case "$input" in
-                "") echo 'Not a vaild Input' ;;
-                'Abort') abort ;;
-                *)
-                  echo "Chosen flake input: $input"
-                  read -n 1 -rp "Would you like to continue? [y/N] " user_input
-                  case "$user_input" in
-                    y|Y) nix flake update "$input" ;;
-                    *) abort
-                  esac
-                  break
-                ;;
-              esac
-            done
-            ;;
-          new|init)
-            if [ -z "$2" ]; then
-              abort "Template name not passed."
-            fi
-
-            if [[ "$1" == "new" ]] && [ -z "$3" ]; then
-              abort "Requires directory."
-            fi
-
-            nix flake "$1" --template "my#templates.$2" $3
-            ;;
-          update) nix flake update --commit-lock-file ;;
-          *) abort "Unknown option." ;;
-        esac
-      '';
-in
-{
-  home.packages = [ mynix ];
-
   programs = {
     nix-index = {
       enable = true;
