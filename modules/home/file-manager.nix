13b8f13e7130e8763fb72bea95d44288a68b930d
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-05 20:37:26 +0530

    feat(home/file-manager)!: stick to defaults
    - rm openers
    - rm open rules
    - rm ratio

--- modules/home/file-manager.nix
+++ modules/home/file-manager.nix
@@ -16,154 +16,21 @@
         enable = true;
         shellWrapperName = "y";
 
+        extraPackages = [
+          pkgs.p7zip # archive (opener.extract)
+        ];
+
         settings = {
-          mgr = {
-            ratio = [
-              1 # left
-              3 # center
-              4 # right
-            ];
-            linemode = "size";
-          };
+          preview.max_width = 1920; # image preview
+          mgr.linemode = "size";
 
-          opener = {
-            edit = [
-              {
-                run = ''${config.my.programs.editor.command} "$@"'';
-                block = true;
-              }
-            ];
-
-            open = [
-              {
-                run = ''xdg-open "$@"'';
-                desc = "Open";
-                for = "linux";
-              }
-            ];
-
-            reveal = [
-              {
-                run = ''xdg-open "$(dirname "$1")"'';
-                desc = "Reveal";
-                for = "linux";
-              }
-            ];
-
-            extract = [
-              {
-                run = ''ya pub extract --list "$@"'';
-                desc = "Extract here";
-                for = "unix";
-              }
-            ];
-
-            play = [
-              {
-                run = ''xdg-open "$@"'';
-                orphan = true;
-                for = "unix";
-              }
-            ];
-          };
-
-          # refer: https://github.com/sxyazi/yazi/blob/shipped/yazi-config/preset/yazi-default.toml#L61
-          open.rules = [
-            # Folder
+          opener.edit = [
             {
-              name = "*/";
-              use = [
-                "edit"
-                "open"
-                "reveal"
-              ];
-            }
-
-            # Text
-            {
-              mime = "text/*";
-              use = [
-                "edit"
-                "reveal"
-              ];
-            }
-
-            # Image
-            {
-              mime = "image/*";
-              use = [
-                "open"
-                "reveal"
-              ];
-            }
-
-            # Media
-            {
-              mime = "{audio,video}/*";
-              use = [
-                "play"
-                "reveal"
-              ];
-            }
-
-            # JSON
-            {
-              mime = "application/{json,ndjson}";
-              use = [
-                "edit"
-                "reveal"
-              ];
-            }
-
-            {
-              mime = "*/javascript";
-              use = [
-                "edit"
-                "reveal"
-              ];
-            }
-
-            # Empty file
-            {
-              mime = "inode/empty";
-              use = [
-                "edit"
-                "reveal"
-              ];
-            }
-
-            # Fallback
-            {
-              name = "*";
-              use = [
-                "open"
-                "reveal"
-              ];
+              run = ''${config.my.programs.editor.command} "$@"'';
+              block = true;
             }
           ];
         };
-      }
-
-      {
-        ## image preview
-        settings.preview = {
-          max_width = 1920;
-        };
-      }
-
-      {
-        # Archive
-        extraPackages = [ pkgs.p7zip ];
-
-        settings.open.rules = [
-          {
-            name = "*.{zip,rar,7z*,tar,gzip,xz,zstd,bzip*,lzma,compress,archive,cpio,arj,xar,ms-cab*}";
-            use = [
-              "extract"
-              "reveal"
-            ];
-          }
-        ];
       }
 
       {
