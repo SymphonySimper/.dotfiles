72aa472c604a4e08417ec366298c68fba49d3d90
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-05 10:12:54 +0530

    feat(profiles/wsl): add win32yank.exe package

--- profiles/wsl/home.nix
+++ profiles/wsl/home.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   cfgSh = config.my.programs.shell;
 
@@ -9,10 +9,6 @@ let
 in
 {
   my.programs = {
-    shell.addToPath = {
-      "win32yank.exe" = "${path.programFiles}/Neovim/bin/win32yank.exe";
-    };
-
     editor.clipboardProvider = "win32-yank";
 
     # NOTE: profile option is just for other settings not starting shell
@@ -24,8 +20,26 @@ in
           --window 0 \
           --profile "NixOS" wsl.exe -d NixOS --cd ~ ${cfgSh.command} ${cfgSh.args.login} ${cfgSh.args.command} "$*"
       '';
-
   };
+
+  home.packages = [
+    (pkgs.stdenv.mkDerivation rec {
+      pname = "win32yank";
+      version = "0.1.1";
+
+      src = pkgs.fetchzip {
+        url = "https://github.com/equalsraf/win32yank/releases/download/${version}/win32yank-x64.zip";
+        sha256 = "sha256-4ivE1cYZhYs4ibx5oiYMOhbse9bdOomk7RjgdVl5lD0=";
+        stripRoot = false;
+      };
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp win32yank.exe $out/bin
+        chmod +x $out/bin/*
+      '';
+    })
+  ];
 
   services.ssh-agent.enable = true;
 }
