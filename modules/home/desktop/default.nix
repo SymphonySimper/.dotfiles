541b0fc1355082d3ec1a3717836de104c6fcf0ef
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-09 16:16:28 +0530

    feat(home/desktop)!: rm services

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -18,8 +18,6 @@ let
   };
 in
 {
-  imports = [ ./services ];
-
   options.my.programs.desktop = {
     enable = (lib.mkEnableOption "Desktop") // {
       default = my.gui.desktop.enable;
--- modules/home/desktop/services/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-{
-  imports = lib.my.mkReadDir {
-    path = ./.;
-    type = "nix";
-    asPath = true;
-    ignoreDefault = true;
-  };
-}
