19f36c5f4b1022139ee4a10e62e8b892ad16b113
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-11 03:21:11 +0530

    refactor(home/work): rm lib.mkMerge

--- modules/home/work.nix
+++ modules/home/work.nix
@@ -1,33 +1,19 @@
+{ config, pkgs, ... }:
 {
-  my,
-  config,
-  pkgs,
-  lib,
-  ...
-}:
-{
-  config = lib.mkMerge [
-    {
-      my.programs = {
-        vcs.profiles."work" = rec {
-          host = "github.com";
-          email = "176003709+smollan-sri-senthil-balaji@users.noreply.github.com";
+  home.packages = [ pkgs.google-cloud-sdk ];
 
-          config.user = {
-            name = "Sri Senthil Balaji J";
-            email = email;
-          };
-        };
+  my.programs = {
+    vcs.profiles."work" = rec {
+      host = "github.com";
+      email = "176003709+smollan-sri-senthil-balaji@users.noreply.github.com";
+
+      config.user = {
+        name = "Sri Senthil Balaji J";
+        email = email;
       };
-    }
+    };
 
-    {
-      home.packages = [ pkgs.google-cloud-sdk ];
-
-      my.programs = {
-        shell.env.GOOGLE_APPLICATION_CREDENTIALS = "${config.xdg.configHome}/gcloud/application_default_credentials.json";
-        editor.ignore = [ "!.gcloudignore" ];
-      };
-    }
-  ];
+    shell.env.GOOGLE_APPLICATION_CREDENTIALS = "${config.xdg.configHome}/gcloud/application_default_credentials.json";
+    editor.ignore = [ "!.gcloudignore" ];
+  };
 }
