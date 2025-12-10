011693dd56257a8f71d3c1d1bbf101f2d10e9f3a
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-11 02:17:33 +0530

    refactor(home/desktop): simplify dconf.settings

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -16,6 +16,22 @@ let
       shift = "<Shift>";
     };
   };
+
+  customKeybinds = (
+    builtins.listToAttrs (
+      lib.lists.imap0 (i: bind: {
+        name = "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom${builtins.toString i}";
+        value = {
+          name = if builtins.hasAttr "name" bind then bind.name else bind.command;
+          binding = builtins.concatStringsSep "" (
+            (lib.lists.optionals (bind.mods != null) (builtins.map (mod: keys.mod.${mod}) bind.mods))
+            ++ [ bind.key ]
+          );
+          command = bind.command;
+        };
+      }) cfg.keybinds
+    )
+  );
 in
 {
   options.my.programs.desktop = {
@@ -134,6 +150,7 @@ in
       settings = lib.mkMerge [
         {
           "org/gnome/mutter" = {
+            overlay-key = "Super_L";
             dynamic-workspaces = true;
 
             experimental-features = [
@@ -197,118 +214,92 @@ in
             remove-old-temp-files = true;
             remove-old-trash-files = true;
           };
+
+          # app folders
+          "org/gnome/desktop/app-folders".folder-children = builtins.attrNames cfg.appfolder;
+
+          # keybinds
+          "org/gnome/desktop/input-sources" = {
+            xkb-options = [
+              "terminate:ctrl_alt_bksp" # default for xkb-options
+              "lv3:rwin_switch" # alternate characters key: setting it to right super to free right alt
+              "ctrl:nocaps" # caps lock as ctrl
+            ];
+          };
+
+          "org/gnome/desktop/wm/keybindings" = {
+            minimize = [ ];
+            close = [ "<Super>q" ]; # close app
+            toggle-maximized = [ "<Super>f" ];
+            toggle-fullscreen = [ "<Super><Shift>f" ];
+
+            switch-windows = [ "<Alt>Tab" ];
+            switch-windows-backward = [ "<Alt><Shift>Tab" ];
+            ## disable switch-applications binding as sometimes it just takes over <Alt>Tab
+            switch-applications = [ ];
+            switch-applications-backward = [ ];
+
+            switch-to-workspace-left = [ "<Super>j" ];
+            switch-to-workspace-right = [ "<Super>k" ];
+            move-to-workspace-left = [ "<Super><Shift>j" ];
+            move-to-workspace-right = [ "<Super><Shift>k" ];
+
+            begin-move = [ "<Super>m" ];
+            begin-resize = [ "<Super>r" ];
+
+            switch-input-source = [ ];
+            switch-input-source-backward = [ ];
+          };
+
+          "org/gnome/shell/keybindings" = {
+            screenshot = [ "<Super>F11" ];
+            show-screenshot-ui = [ "<Super><Shift>F11" ];
+            show-screen-recording-ui = [ "<Super><Control>F11" ];
+          };
+
+          "org/gnome/settings-daemon/plugins/media-keys" = {
+            mic-mute = [ "F8" ];
+            play = [ "F7" ];
+            volume-down = [ "<Super>F2" ];
+            volume-mute = [ "<Super><Shift>F2" ];
+            volume-up = [ "<Super>F3" ];
+
+            screensaver = [ "<Super>Escape" ];
+            logout = [ "<Super><Control>Escape" ];
+            shutdown = [ "<Super><Alt>Escape" ];
+            reboot = [ "<Super><Alt><Control>Escape" ];
+
+            home = [ "<Super><Shift>e" ];
+            www = [ "<Super>b" ]; # launch browser
+
+            custom-keybindings = builtins.map (name: "/${name}/") (builtins.attrNames customKeybinds);
+          };
+
+          "org/gnome/mutter/wayland/keybindings" = {
+            restore-shortcuts = [ ];
+          };
         }
 
-        (lib.mkMerge [
-          # App folders
-          (builtins.listToAttrs (
-            builtins.map (folder: {
-              name = "org/gnome/desktop/app-folders/folders/${folder.name}";
-              value = lib.mkMerge [
-                { name = folder.name; }
+        # App folders
+        (builtins.listToAttrs (
+          builtins.map (folder: {
+            name = "org/gnome/desktop/app-folders/folders/${folder.name}";
+            value = lib.mkMerge [
+              { name = folder.name; }
 
-                (lib.mkIf (builtins.length folder.value.apps > 0) {
-                  apps = folder.value.apps;
-                })
+              (lib.mkIf (builtins.length folder.value.apps > 0) {
+                apps = folder.value.apps;
+              })
 
-                (lib.mkIf (builtins.length folder.value.categories > 0) {
-                  categories = folder.value.categories;
-                })
-              ];
-            }) (lib.attrsets.attrsToList cfg.appfolder)
-          ))
+              (lib.mkIf (builtins.length folder.value.categories > 0) {
+                categories = folder.value.categories;
+              })
+            ];
+          }) (lib.attrsets.attrsToList cfg.appfolder)
+        ))
 
-          { "org/gnome/desktop/app-folders".folder-children = builtins.attrNames cfg.appfolder; }
-        ])
-
-        (
-          let
-            customKeybinds = (
-              builtins.listToAttrs (
-                lib.lists.imap0 (i: bind: {
-                  name = "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom${builtins.toString i}";
-                  value = {
-                    name = if builtins.hasAttr "name" bind then bind.name else bind.command;
-                    binding = builtins.concatStringsSep "" (
-                      (lib.lists.optionals (bind.mods != null) (builtins.map (mod: keys.mod.${mod}) bind.mods))
-                      ++ [ bind.key ]
-                    );
-                    command = bind.command;
-                  };
-                }) cfg.keybinds
-              )
-            );
-          in
-          lib.mkMerge [
-            # Keybinds
-            {
-              "org/gnome/mutter".overlay-key = "Super_L";
-
-              "org/gnome/desktop/input-sources" = {
-                xkb-options = [
-                  "terminate:ctrl_alt_bksp" # default for xkb-options
-                  "lv3:rwin_switch" # alternate characters key: setting it to right super to free right alt
-                  "ctrl:nocaps" # caps lock as ctrl
-                ];
-              };
-
-              "org/gnome/desktop/wm/keybindings" = {
-                minimize = [ ];
-                close = [ "<Super>q" ]; # close app
-                toggle-maximized = [ "<Super>f" ];
-                toggle-fullscreen = [ "<Super><Shift>f" ];
-
-                switch-windows = [ "<Alt>Tab" ];
-                switch-windows-backward = [ "<Alt><Shift>Tab" ];
-                ## disable switch-applications binding as sometimes it just takes over <Alt>Tab
-                switch-applications = [ ];
-                switch-applications-backward = [ ];
-
-                switch-to-workspace-left = [ "<Super>j" ];
-                switch-to-workspace-right = [ "<Super>k" ];
-                move-to-workspace-left = [ "<Super><Shift>j" ];
-                move-to-workspace-right = [ "<Super><Shift>k" ];
-
-                begin-move = [ "<Super>m" ];
-                begin-resize = [ "<Super>r" ];
-
-                switch-input-source = [ ];
-                switch-input-source-backward = [ ];
-              };
-
-              "org/gnome/shell/keybindings" = {
-                screenshot = [ "<Super>F11" ];
-                show-screenshot-ui = [ "<Super><Shift>F11" ];
-                show-screen-recording-ui = [ "<Super><Control>F11" ];
-              };
-
-              "org/gnome/settings-daemon/plugins/media-keys" = {
-                mic-mute = [ "F8" ];
-                play = [ "F7" ];
-                volume-down = [ "<Super>F2" ];
-                volume-mute = [ "<Super><Shift>F2" ];
-                volume-up = [ "<Super>F3" ];
-
-                screensaver = [ "<Super>Escape" ];
-                logout = [ "<Super><Control>Escape" ];
-                shutdown = [ "<Super><Alt>Escape" ];
-                reboot = [ "<Super><Alt><Control>Escape" ];
-
-                home = [ "<Super><Shift>e" ];
-                www = [ "<Super>b" ]; # launch browser
-
-                custom-keybindings = builtins.map (name: "/${name}/") (builtins.attrNames customKeybinds);
-              };
-
-              "org/gnome/mutter/wayland/keybindings" = {
-                restore-shortcuts = [ ];
-              };
-            }
-
-            ## Custom Keybinds
-            customKeybinds
-          ]
-        )
+        ## Custom Keybinds
+        customKeybinds
       ];
     };
 
