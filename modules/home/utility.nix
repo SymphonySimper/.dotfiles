00d0f6192ff0b64260b2074fdf3d779d51a257d6
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-11 00:44:54 +0530

    refactor(home/utility): rm lib.mkMerge

--- modules/home/utility.nix
+++ modules/home/utility.nix
@@ -5,49 +5,39 @@
   ...
 }:
 {
-  config = lib.mkMerge [
-    {
-      home.packages = with pkgs; [ bc ];
-    }
+  home.packages = builtins.concatLists [
+    [ pkgs.bc ]
 
-    {
-      programs.man = {
-        enable = true;
-        generateCaches = true;
-      };
-    }
-
-    {
-      programs.fzf = {
-        enable = true;
-        defaultOptions = [ "--reverse" ];
-      };
-    }
-
-    {
-      programs.btop = {
-        enable = true;
-
-        package = pkgs.btop.override {
-          # GPU support
-          rocmSupport = true; # amd
-          cudaSupport = true; # nvida
-        };
-
-        settings = {
-          vim_keys = true;
-          shown_boxes = "cpu mem net proc gpu0";
-          update_ms = 2000; # recommended
-          proc_tree = false;
-        };
-      };
-    }
-
-    {
-      # Clipboard
-      home.packages = lib.lists.optional (
-        my.gui.enable && pkgs.stdenv.hostPlatform.isLinux
-      ) pkgs.wl-clipboard;
-    }
+    # Clipboard
+    (lib.lists.optional (my.gui.enable && pkgs.stdenv.hostPlatform.isLinux) pkgs.wl-clipboard)
   ];
+
+  programs = {
+    man = {
+      enable = true;
+      generateCaches = true;
+    };
+
+    fzf = {
+      enable = true;
+      defaultOptions = [ "--reverse" ];
+    };
+
+    btop = {
+      enable = true;
+
+      package = pkgs.btop.override {
+        # GPU support
+        rocmSupport = true; # amd
+        cudaSupport = true; # nvida
+      };
+
+      settings = {
+        vim_keys = true;
+        shown_boxes = "cpu mem net proc gpu0";
+        update_ms = 2000; # recommended
+        proc_tree = false;
+      };
+    };
+  };
 }
