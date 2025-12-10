0b661cff39232c0c2b0307bcb3b77c3dd4d31064
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-10 22:09:22 +0530

    refactor(home/theme): rm lib.mkMerge

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -5,39 +5,33 @@
   ...
 }:
 {
-  config = lib.mkIf my.gui.enable (
-    lib.mkMerge [
-      {
-        home.packages = with pkgs; [
-          noto-fonts-cjk-sans
+  config = lib.mkIf my.gui.enable {
+    home.packages = with pkgs; [
+      noto-fonts-cjk-sans
 
-          (nerd-fonts.jetbrains-mono.overrideAttrs {
-            # refer: https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerd-fonts/default.nix#L65
-            preInstall =
-              let
-                variants = builtins.concatStringsSep "\\|" [
-                  "Regular"
-                  "Italic"
-                  "Bold.*"
-                ];
-              in
-              # sh
-              ''
-                find . -type f -not -regex ".*JetBrainsMonoNerdFont-\(${variants}\).ttf" -delete
-              '';
-          })
-        ];
-      }
+      (nerd-fonts.jetbrains-mono.overrideAttrs {
+        # refer: https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerd-fonts/default.nix#L65
+        preInstall =
+          let
+            variants = builtins.concatStringsSep "\\|" [
+              "Regular"
+              "Italic"
+              "Bold.*"
+            ];
+          in
+          # sh
+          ''
+            find . -type f -not -regex ".*JetBrainsMonoNerdFont-\(${variants}\).ttf" -delete
+          '';
+      })
+    ];
 
-      {
-        dconf = {
-          enable = true;
+    dconf = {
+      enable = true;
 
-          settings."org/gnome/desktop/interface" = {
-            color-scheme = if my.theme.dark then "prefer-dark" else "default";
-          };
-        };
-      }
-    ]
-  );
+      settings."org/gnome/desktop/interface" = {
+        color-scheme = if my.theme.dark then "prefer-dark" else "default";
+      };
+    };
+  };
 }
