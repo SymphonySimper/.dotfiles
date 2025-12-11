238a366005dbd2a4b44da5e18fbf867498368884
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-11 19:39:39 +0530

    refactor(scripts/ffmpeg): replace app variable with nix variable

--- modules/home/scripts/ffmpeg.nix
+++ modules/home/scripts/ffmpeg.nix
@@ -6,6 +6,8 @@
 }:
 let
   cfg = config.my.programs.scripts.ffmpeg;
+
+  app = lib.getExe' pkgs.ffmpeg "ffmpeg";
 in
 {
   options.my.programs.scripts.ffmpeg.enable = lib.mkEnableOption "FFMPEG";
@@ -15,8 +17,6 @@ in
       (pkgs.writeShellScriptBin "myffmpeg"
         # sh
         ''
-          app= "${lib.getExe' pkgs.ffmpeg "ffmpeg"}"
-
           function get_filename() {
             echo $1 | cut -d '.' -f1
           }
@@ -24,18 +24,18 @@ in
           case "$1" in
           shrink)
             shift;
-            $app -i $1 -vcodec libx265 -crf 28 "$(get_filename $1)_shrunk.mp4"
+            ${app} -i $1 -vcodec libx265 -crf 28 "$(get_filename $1)_shrunk.mp4"
             ;;
           to_gif)
             shift;
-            $app -i $1 \
+            ${app} -i $1 \
             -vf "fps=10,scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
             -loop 0 "$(get_filename $1).gif"
             ;;
           to_webm)
             shift;
 
-            $app -i $1 -c:v libvpx-vp9 -b:v 0 -crf 30 -c:a libopus -b:a 64k "$(get_filename $1)_webm.webm"
+            ${app} -i $1 -c:v libvpx-vp9 -b:v 0 -crf 30 -c:a libopus -b:a 64k "$(get_filename $1)_webm.webm"
             ;;
           *) echo "Unknown option" ;;
           esac
