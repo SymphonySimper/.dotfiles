3cd994bb91d0375ce9438927b31b14472f9515ef
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-12-15 13:53:55 +0530

    feat(home/mux)!: rm keybinds and command option
    Needless complexity

--- modules/home/mux.nix
+++ modules/home/mux.nix
@@ -22,84 +22,11 @@ let
   );
 in
 {
-  options.my.programs.mux =
-    (lib.my.mkCommandOption {
-      category = "Terminal Mux";
-      command = "tmux";
-      args = {
-        new = "new";
-      };
-    })
-    // {
-      enable = lib.mkEnableOption "Terminal MUX";
-
-      keybinds = lib.mkOption {
-        description = "MUX keybinds";
-        type = lib.types.listOf (
-          lib.types.submodule {
-            options = {
-              repeat = lib.mkEnableOption "Repeat key";
-
-              unbind = lib.mkEnableOption "Unbid key";
-              key = lib.mkOption {
-                description = "Key for action";
-                type = lib.types.str;
-              };
-
-              action = lib.mkOption {
-                description = "Action to perfom on keybind";
-                type = lib.types.enum [
-                  "new-window"
-                  "run-window"
-                  "split-window"
-
-                  "run-shell"
-                  "display-popup"
-                ];
-              };
-              cd = lib.mkEnableOption "CD";
-              args = lib.mkOption {
-                description = "Args for action";
-                type = lib.types.listOf lib.types.str;
-                default = [ ];
-              };
-            };
-          }
-        );
-        default = [ ];
-      };
-    };
+  options.my.programs.mux = {
+    enable = lib.mkEnableOption "Terminal MUX";
+  };
 
   config = lib.mkIf cfg.enable {
-    my.programs = {
-      mux.keybinds = [
-        # Open history in editor
-        {
-          key = "e";
-          action = "run-shell";
-          args = [ history ];
-        }
-
-        # split pane commands and open panes in cwd
-        {
-          key = "-";
-          action = "split-window";
-          cd = true;
-        }
-        {
-          key = "|";
-          action = "split-window";
-          cd = true;
-          args = [ "-h" ];
-        }
-        {
-          key = "c";
-          action = "new-window";
-          cd = true;
-        }
-      ];
-    };
-
     catppuccin.tmux.extraConfig = # conf
       ''
         ## status
@@ -133,63 +60,41 @@ in
         customPaneNavigationAndResize = true;
         newSession = false;
 
-        extraConfig = lib.strings.concatLines (
-          builtins.concatLists [
-            [
-              # conf
-              ''
-                set -g default-command "exec ${config.my.programs.shell.command}"
+        extraConfig = # conf
+          ''
+            set -g default-command "exec ${config.my.programs.shell.command}"
 
-                # RGB colors
-                # https://github.com/tmux/tmux/wiki/FAQ#how-do-i-use-rgb-colour
-                set -as terminal-features ",${defaultTerminal}:RGB"
+            # RGB colors
+            # https://github.com/tmux/tmux/wiki/FAQ#how-do-i-use-rgb-colour
+            set -as terminal-features ",${defaultTerminal}:RGB"
 
-                setw -g monitor-activity on
-                set -g visual-activity off # If enabled shows activity in window message
+            setw -g monitor-activity on
+            set -g visual-activity off # If enabled shows activity in window message
 
-                # UI
-                ## window
-                set -g renumber-window on # renumber when window is closed
-                set -g window-status-separator "" # remove gap between window text
+            # UI
+            ## window
+            set -g renumber-window on # renumber when window is closed
+            set -g window-status-separator "" # remove gap between window text
 
-                # Keybinds
-                ## y and p as in vim
-                bind Escape copy-mode
-                unbind p
-                bind p paste-buffer
-                bind-key -T copy-mode-vi "v" send -X begin-selection
-                bind-key -T copy-mode-vi "y" send -X copy-selection
+            # Keybinds
+            ## y and p as in vim
+            bind Escape copy-mode
+            unbind p
+            bind p paste-buffer
+            bind-key -T copy-mode-vi "v" send -X begin-selection
+            bind-key -T copy-mode-vi "y" send -X copy-selection
 
-                ## Create new session
-                bind -r C-n new
-              ''
-            ]
+            ## Create new session
+            bind -r C-n new
 
-            (builtins.concatMap (
-              keybind:
-              (lib.optionals keybind.unbind [
-                "unbind ${keybind.key}"
-              ])
-              ++ [
-                (builtins.concatStringsSep " " (
-                  [
-                    "bind"
-                    (lib.strings.optionalString keybind.repeat "-r")
-                    keybind.key
-                    keybind.action
-                    (lib.strings.optionalString keybind.cd (
-                      let
-                        arg = if keybind.action == "display-popup" then "-d" else "-c";
-                      in
-                      "${arg} \"#{pane_current_path}\""
-                    ))
-                  ]
-                  ++ keybind.args
-                ))
-              ]
-            ) cfg.keybinds)
-          ]
-        );
+            ## split pane commands and open panes in cwd
+            bind  - split-window -c "#{pane_current_path}"
+            bind  | split-window -c "#{pane_current_path}" -h
+            bind  c new-window -c "#{pane_current_path}"
+
+            ## Open history in editor
+            bind  e run-shell  ${history}
+          '';
       };
     };
   };
