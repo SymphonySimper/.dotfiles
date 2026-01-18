5abcaf18a769486e4b020585431f98992a0b1900
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-01-18 19:39:41 +0530

    feat(profiles/gui): enable gh module and add script to run nixpkgs-review

--- profiles/gui/home.nix
+++ profiles/gui/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   my.programs = {
     vcs.profiles.gnome = rec {
@@ -8,9 +8,27 @@
     };
   };
 
-  programs.discord.enable = true;
+  programs = {
+    discord.enable = true;
 
-  home.packages = with pkgs; [
-    libreoffice
+    gh = {
+      enable = true;
+      settings.git_protocol = "ssh";
+    };
+  };
+
+  home.packages = [
+    pkgs.libreoffice
+
+    (pkgs.writeShellScriptBin "mynixpkgsreview" ''
+      pr_id="$1"
+
+      if [ -z "$1" ]; then
+        echo "Requires PR ID (ex: 481226)."
+        exit 1
+      fi
+
+      ${lib.getExe' pkgs.nixpkgs-review "nixpkgs-review"} pr --post-result "$pr_id"
+    '')
   ];
 }
