0b753e317a235823ed2ec05cf51ece1f31a25c21
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-07-05 19:30:44 +0530

    fix(home/direnv): keep config under config

--- modules/home/direnv.nix
+++ modules/home/direnv.nix
@@ -5,17 +5,19 @@ in
 {
   options.my.programs.direnv.enable = lib.mkEnableOption "Enable Direnv";
 
-  my.programs.editor.ignore = [
-    # direnv
-    "!.envrc"
-    ".direnv"
-  ];
+  config = {
+    my.programs.editor.ignore = [
+      # direnv
+      "!.envrc"
+      ".direnv"
+    ];
 
-  direnv = lib.mkIf cfg.enable {
-    enable = true;
-    nix-direnv.enable = true;
+    programs.direnv = lib.mkIf cfg.enable {
+      enable = true;
+      nix-direnv.enable = true;
 
-    silent = false;
-    config.warn_timeout = "2m";
+      silent = false;
+      config.warn_timeout = "2m";
+    };
   };
 }
