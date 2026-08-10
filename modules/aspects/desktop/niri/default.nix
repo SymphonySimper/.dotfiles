a47a2450d5db30e98fafb5af6f804d8da7315388
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-08-10 09:15:14 +0530

    temp

--- modules/aspects/desktop/niri/default.nix
+++ modules/aspects/desktop/niri/default.nix
@@ -6,13 +6,37 @@
       programs.niri.enable = true;
     };
 
-    homeManager = {
-      wayland.windowManager.niri = {
-        enable = true;
-        xwaylandSatellitePackage = null; # disable xwayland
-      };
+    homeManager =
+      let
+        pointerAccel = {
+          accel-speed = 0;
+          accel-profile = "flat";
+        };
+      in
+      {
+        programs.fuzzel.enable = true;
 
-      programs.fuzzel.enable = true;
-    };
+        wayland.windowManager.niri = {
+          enable = true;
+          xwaylandSatellitePackage = null; # disable xwayland
+
+          settings = {
+            input = {
+              keyboard.xkb = {
+                options = "ctrl:nocaps";
+              };
+
+              mouse = pointerAccel;
+
+              touchpad = {
+                tap = { };
+              }
+              // pointerAccel;
+            };
+          };
+
+          animations.off = { };
+        };
+      };
   };
 }
