82233eaee7f3a6976aec951b3b954b9e3114d955
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-09-05 01:07:08 +0530

    virtualisation/virt-manager: configure host services
    Remove SPICE guest agents from the host configuration. Register virtiofsd as a libvirt vhost-user package so libvirt can discover it.

--- modules/nixos/virtualisation/virt-manager.nix
+++ modules/nixos/virtualisation/virt-manager.nix
@@ -9,13 +9,9 @@
   config = lib.mkIf config.programs.virt-manager.enable {
     users.groups.libvirtd.members = config.users.groups.wheel.members;
 
-    virtualisation.libvirtd.enable = true;
-
-    services = {
-      spice-webdavd.enable = true;
-      spice-vdagentd.enable = true;
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu.vhostUserPackages = [ pkgs.virtiofsd ];
     };
-
-    environment.systemPackages = with pkgs; [ virtiofsd ];
   };
 }
