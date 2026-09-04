b09d84b1a88d0953273038f933bb16217d2f5f01
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-09-05 01:11:55 +0530

    virtualisation/docker: remove rootful daemon policy
    Leave daemon selection to host configurations. Point the Docker CLI at the user socket when rootless Docker is enabled.

--- modules/nixos/virtualisation/docker.nix
+++ modules/nixos/virtualisation/docker.nix
@@ -1,16 +1,6 @@
-{ config, lib, ... }:
+{ ... }:
 {
-  config = lib.mkIf config.virtualisation.docker.enable {
-    virtualisation.docker = {
-      enableOnBoot = lib.mkDefault false;
-      liveRestore = false;
-
-      autoPrune.enable = true;
-
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-      };
-    };
+  virtualisation.docker.rootless = {
+    setSocketVariable = true;
   };
 }
