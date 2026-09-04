76cfc74b5b56df05f8e67840953a1b8fca2daa8a
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-09-05 03:04:35 +0530

    nixos/desktop: reformat module

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -13,33 +13,30 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    services = {
-      desktopManager.gnome.enable = true;
-      displayManager.gdm.enable = true;
-    };
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-    environment = {
-      sessionVariables.NIXOS_OZONE_WL = "1";
+    services.desktopManager.gnome.enable = true;
+    services.displayManager.gdm.enable = true;
 
-      gnome.excludePackages = with pkgs; [
-        baobab # disk usage analyzer
-        epiphany # browser
-        geary # email
-        gnome-connections
-        gnome-console
-        gnome-contacts
-        gnome-font-viewer
-        gnome-logs
-        gnome-maps
-        gnome-music
-        gnome-system-monitor
-        gnome-text-editor
-        gnome-tour
-        gnome-weather
-        simple-scan
-        sushi # nautilus preview
-        yelp
-      ];
-    };
+
+    environment.gnome.excludePackages = with pkgs; [
+      baobab # disk usage analyzer
+      epiphany # browser
+      geary # email
+      gnome-connections
+      gnome-console
+      gnome-contacts
+      gnome-font-viewer
+      gnome-logs
+      gnome-maps
+      gnome-music
+      gnome-system-monitor
+      gnome-text-editor
+      gnome-tour
+      gnome-weather
+      simple-scan
+      sushi # nautilus preview
+      yelp
+    ];
   };
 }
