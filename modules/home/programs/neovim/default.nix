25d58858689ac099c9339e4bc7e41b46ffaf9005
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-09-07 20:54:34 +0530

    remove config.lua

--- modules/home/programs/neovim/default.nix
+++ modules/home/programs/neovim/default.nix
@@ -9,29 +9,17 @@
     ./tree-sitter.nix
   ];
 
-  options.programs.neovim = {
-    config.lua = lib.mkOption {
-      type = lib.types.lines;
-      description = "Config to be added to init.lua";
-      default = "";
-    };
-  };
+  programs.neovim = {
+    enable = true;
+    # defaultEditor = false;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    waylandSupport = config.desktop.enable;
 
-  config = {
-    programs.neovim = {
-      enable = true;
-      # defaultEditor = false;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      waylandSupport = config.desktop.enable;
-
-      withNodeJs = lib.mkForce false;
-      withPerl = lib.mkForce false;
-      withRuby = lib.mkForce false;
-      withPython3 = lib.mkForce false;
-
-      initLua = config.programs.neovim.config.lua;
-    };
+    withNodeJs = lib.mkForce false;
+    withPerl = lib.mkForce false;
+    withRuby = lib.mkForce false;
+    withPython3 = lib.mkForce false;
   };
 }
