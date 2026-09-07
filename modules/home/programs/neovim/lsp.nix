1964598699ad66268291cb4c5f2b89c29c539127
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-09-07 20:04:34 +0530

    neovim: use native completion and tree-sitter highlighting

--- modules/home/programs/neovim/lsp.nix
+++ modules/home/programs/neovim/lsp.nix
@@ -16,7 +16,21 @@
       {
         plugin = pkgs.vimPlugins.nvim-lspconfig;
         type = "lua";
-        config = config.programs.neovim.config.lsp; # refer: https://github.com/neovim/nvim-lspconfig#quickstart
+        config = ''
+          vim.opt.completeopt:append({ "fuzzy", "menuone", "noselect", "popup" })
+
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+              if client:supports_method("textDocument/completion") then
+                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+              end
+            end,
+          })
+
+          ${config.programs.neovim.config.lsp}
+        ''; # refer: https://github.com/neovim/nvim-lspconfig#quickstart
       }
     ];
   };
