0f0a3e52b37f7eae9ac7bdace9ad071e1d3d656f
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2026-09-10 10:43:15 +0530

    programs/tmux: remove #conf tree-sitter injection

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -16,25 +16,24 @@ let
   '';
 in
 {
-  catppuccin.tmux.extraConfig = # conf
-    ''
-      ## status
-      set -g status on
-      set -g status-position bottom
-      set -g status-left ""
-      set -g status-right ""
-      set -g status-interval 5
+  catppuccin.tmux.extraConfig = ''
+    ## status
+    set -g status on
+    set -g status-position bottom
+    set -g status-left ""
+    set -g status-right ""
+    set -g status-interval 5
 
-      set -g @catppuccin_window_status_style "basic"
+    set -g @catppuccin_window_status_style "basic"
 
-      setw -g automatic-rename on
-      setw -g automatic-rename-format "#{pane_current_path}: #{pane_current_command}"
+    setw -g automatic-rename on
+    setw -g automatic-rename-format "#{pane_current_path}: #{pane_current_command}"
 
-      set -g @catppuccin_window_text " #{pane_current_command}"
-      set -g @catppuccin_window_current_text " #{pane_current_command}"
+    set -g @catppuccin_window_text " #{pane_current_command}"
+    set -g @catppuccin_window_current_text " #{pane_current_command}"
 
-      set -ag status-right "#{E:@catppuccin_status_session}"
-    '';
+    set -ag status-right "#{E:@catppuccin_status_session}"
+  '';
 
   programs.tmux = {
     enable = lib.mkDefault true;
@@ -51,44 +50,43 @@ in
       if config.programs.fish.enable then config.programs.fish.package else config.programs.bash.package
     );
 
-    extraConfig = # conf
-      ''
-        # RGB colors
-        # https://github.com/tmux/tmux/wiki/FAQ#how-do-i-use-rgb-colour
-        set -as terminal-features ",${cfg.terminal}:RGB"
+    extraConfig = ''
+      # RGB colors
+      # https://github.com/tmux/tmux/wiki/FAQ#how-do-i-use-rgb-colour
+      set -as terminal-features ",${cfg.terminal}:RGB"
 
-        setw -g monitor-activity on
-        set -g visual-activity off # If enabled shows activity in window message
+      setw -g monitor-activity on
+      set -g visual-activity off # If enabled shows activity in window message
 
-        # Required by yazi
-        set -g allow-passthrough on
-        set -ga update-environment TERM
-        set -ga update-environment TERM_PROGRAM
+      # Required by yazi
+      set -g allow-passthrough on
+      set -ga update-environment TERM
+      set -ga update-environment TERM_PROGRAM
 
-        # UI
-        ## window
-        set -g renumber-window on # renumber when window is closed
-        set -g window-status-separator "" # remove gap between window text
+      # UI
+      ## window
+      set -g renumber-window on # renumber when window is closed
+      set -g window-status-separator "" # remove gap between window text
 
-        # Keybinds
-        ## y and p as in vim
-        bind Escape copy-mode
-        unbind p
-        bind p paste-buffer
-        bind-key -T copy-mode-vi "v" send -X begin-selection
-        bind-key -T copy-mode-vi "y" send -X copy-selection
+      # Keybinds
+      ## y and p as in vim
+      bind Escape copy-mode
+      unbind p
+      bind p paste-buffer
+      bind-key -T copy-mode-vi "v" send -X begin-selection
+      bind-key -T copy-mode-vi "y" send -X copy-selection
 
-        ## Create new session
-        bind -r C-n new
+      ## Create new session
+      bind -r C-n new
 
-        ## split pane commands and open panes in cwd
-        bind - split-window -c "#{pane_current_path}"
-        bind | split-window -c "#{pane_current_path}" -h
-        bind c new-window -c "#{pane_current_path}"
+      ## split pane commands and open panes in cwd
+      bind - split-window -c "#{pane_current_path}"
+      bind | split-window -c "#{pane_current_path}" -h
+      bind c new-window -c "#{pane_current_path}"
 
-        ## Open External programs
-        bind e run-shell  ${history} # Open history in editor
-      '';
+      ## Open External programs
+      bind e run-shell  ${history} # Open history in editor
+    '';
   };
 
   programs.kitty.settings.shell = if cfg.enable then lib.getExe cfg.package else ".";
